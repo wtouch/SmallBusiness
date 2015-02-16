633468914369f19b13b4f9df377ecd24dde22f05
Trupti Misal <trupti@wtouch.in>
2015-02-16 16:00:51 +0530

    edited

--- app/modules/dashboard/dashboardController.js
+++ app/modules/dashboard/dashboardController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector'];
+    var injectParams = ['$scope', '$injector','$routParams','$http'];
 
     // This is controller for this view
 	var dashboardController = function ($scope, $injector) {
--- app/modules/login/logincontroller.js
+++ app/modules/login/logincontroller.js
@@ -2,11 +2,12 @@
 'use strict';
 
 define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector','$http'];
+    var injectParams = ['$scope', '$injector'];
 
     // This is controller for this view
-	var loginController = function ($scope,$injector,$http) {
+	var loginController = function ($scope,$injector) {
 		console.log("this is login ctrl");
+		templateUrl:'http://localhost/trupti/SmallBusiness/app/modules/dashboard/dashboard.html';
     };
 	// Inject controller's dependencies
 	loginController.$inject = injectParams;
