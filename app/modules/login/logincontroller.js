c3ffce84511c17249a1e66bccd60965b712e22a0
Trupti Misal <trupti@wtouch.in>
2015-02-16 11:25:43 +0530

    login.html edited

--- app/modules/login/logincontroller.js
+++ app/modules/login/logincontroller.js
@@ -1,18 +1,15 @@
 
-
 'use strict';
 
 define(['app'], function (app) {
     var injectParams = ['$scope', '$injector','$http'];
 
     // This is controller for this view
-	var loginController = function ($scope, $injector,$http) {
-		console.log("this is home ctrl");
+	var loginController = function ($scope,$injector,$http) {
+		console.log("this is login ctrl");
     };
 	// Inject controller's dependencies
 	loginController.$inject = injectParams;
 	// Register/apply controller dynamically
     app.register.controller('loginController', loginController);
-	
-	
 });
