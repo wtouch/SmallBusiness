818aed58c2b4c940d6db7f3fba828aa80a80d423
Aarti Kulkarni <aarti@wtouch.in>
2015-02-22 15:49:09 +0530

    add function for multiview

--- app/modules/websites/mywebsitesController.js
+++ app/modules/websites/mywebsitesController.js
@@ -3,11 +3,15 @@
 'use strict';
 
 define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector'];
+    var injectParams = ['$scope', '$injector','$routeParams','$location'];
 
     // This is controller for this view
-	var mywebsitesController = function ($scope, $injector) {
+	var mywebsitesController = function ($scope, $injector,$routeParams,$location) {
 		console.log("this is mywebsites ctrl ");
+		$scope.newPage=function(){
+			location.href='#/mywebsites.html';
+			console.log("Hi");
+		}
     };
 	
     
