fbeaa898ad30d41c5e156058b035cc34917e3956
Trupti Misal <trupti@wtouch.in>
2015-02-25 16:47:27 +0530

    manageuserController.js edited

--- app/modules/users/manageuser/manageuserController.js
+++ app/modules/users/manageuser/manageuserController.js
@@ -1,16 +1,29 @@
-
 'use strict';
 
-define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector'];
-
+define(['app'], function (app) { 
+    var injectParams = ['$scope', '$injector', '$routeParams']; /* Added $routeParams to access route parameters */
     // This is controller for this view
-	var manageuserController = function ($scope,$injector) {
-	//	console.log("this is login ctrl");
-		templateUrl:'http://localhost/trupti/SmallBusiness/app/modules/dashboard/dashboard.html';
+	var manageController = function ($scope, $injector, $routeParams) {
+		console.log("this is manage Controller");
+		$scope.MailView = $routeParams.mailId; /* this object will check list of mails show or single mail show */
+		
+		templateUrl:'http://localhost/trupti/SmallBusiness/app/modules/dashboard/dashboard/enquiry/mailview.html';
+		$scope.goBack = function() 
+		{
+			window.history.back();
+		};
+		
+		$scope.newPage = function (){
+			location.href = '#/template.html';
+			consol.log("Hiii");
+		};
+		
     };
+	
 	// Inject controller's dependencies
-	manageuserController.$inject = injectParams;
+	enquiryController.$inject = injectParams;
 	// Register/apply controller dynamically
-    app.register.controller('manageuserController', manageuserController);
+    app.register.controller('enquiryController', enquiryController);
+	
+	
 });
