76dfe76ec4c24456a01869db0947028d29f6bcea
Sonali Shinde <sonali@wtouch.in>
2015-03-02 12:31:58 +0530

    server-api updated

---
+++ Server-api/modules/website.php
@@ -0,0 +1,32 @@
+<?php
+	require_once 'db/dbHelper.php';
+	$db = new dbHelper();
+	$reqMethod = $app->request->getMethod();
+	
+	if($reqMethod=="GET"){
+		echo $reqMethod;
+		if(isset($id)){
+			$where['id'] = $id;
+			$data = $db->select("2_real_property", $where);
+			echo json_encode($data);
+			
+		}else{
+			echo $pageNo;
+			echo $records;
+			$where=[];
+			$limit = $pageNo.",".$records;
+			$data = $db->select("2_real_property", $where, $limit);
+			echo json_encode($data);
+		}
+	}
+	
+	if($reqMethod=="POST"){
+		echo $reqMethod;
+		echo $body;
+	}
+	
+	if($reqMethod=="PUT" || $reqMethod=="DELETE"){
+		echo $reqMethod;
+		echo $body;
+	}
+ ?>
