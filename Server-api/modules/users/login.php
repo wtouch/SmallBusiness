97faa2d5c4ae6d4262de50bec233a13ed37b76a4
Trupti Misal <trupti@wtouch.in>
2015-03-02 18:16:29 +0530

    add methods to business.php
    add methods to enquiry.php
    add methods to mail.php
    add methods to login.php
    add methods to register.php

--- Server-api/modules/users/login.php
+++ Server-api/modules/users/login.php
@@ -1,3 +1,10 @@
 <?php
-
+	require_once 'db/dbHelper.php';
+	$db = new dbHelper();
+	$reqMethod = $app->request->getMethod();
+	
+	if($reqMethod=="POST"){
+		echo $reqMethod;
+		echo $body;
+	}
  ?>
