c1b6f140e7f6f64beff0c5c8cd2bcc288d10d10b
Sonali Shinde <sonali@wtouch.in>
2015-03-08 17:27:57 +0530

    add edit btn code in businessController

--- app/modules/business/businessController.js
+++ app/modules/business/businessController.js
@@ -6,37 +6,86 @@ define(['app'], function (app) {
     // This is controller for this view
 	var businessController = function ($scope, $injector, $routeParams,$location,dataService)
 	{
-		//method for insert data of add business form{trupti}
-		$scope.insert = function(addbusiness){
-			console.log($scope.addbusiness);
-			dataService.post("../server-api/index.php/post/business",$scope.addbusiness)
-			.then(function(response) {
-				alert(response);
-				console.log(response);
-			})
-		}	//end of insert
+		//all $scope object goes here
+		$scope.maxSize = 5;
+		$scope.totalRecords = "";
+		$scope.bizListCurrentPage = 1;
+		$scope.delBizCurrentPage = 1;
+		$scope.pageItems = 10;
+		$scope.numPages = "";	
+		$scope.user_id = {user_id : 2}; // these are URL parameters
+		$scope.formPart = $routeParams.formPart;
 		
-		//This code for publish unpublish button{sonali}
+		
+		// This will change businessView dynamically from 'business.html' {Vilas}
+		
+		$scope.businessView = $routeParams.businessView;
+		console.log($scope.businessView );
+				
+		// Add Business multi part form show/hide operation from here! {Vilas}
+		$scope.formPart = 'home';
+		console.log($scope.formPart);
+		$scope.showFormPart = function(formPart){
+			$scope.formPart = formPart;
+		};  
+		
+		//for display default businesslist.html{trupti}
+		if(!$routeParams.businessView) {
+			$location.path('/dashboard/business/businesslist');
+		}
+				
+		$scope.pageChanged = function(page) {
+			//$log.log('Page changed to: ' + $scope.currentPage);
+			//get request for businesslist
+			dataService.get("/getmultiple/business/"+page+"/"+$scope.pageItems)
+			.then(function(response) { //function for businesslist response
+				$scope.bizList = response.data;
+				//$scope.totalRecords = response.totalRecords;
+				
+			});
+			//get request for delete bizlist 
+			dataService.get("/getmultiple/business/"+page+"/"+$scope.pageItems)
+			.then(function(response) { //function for deltebiz response
+				$scope.delBiz = response.data;
+				//$scope.totalRecords = response.totalRecords;
+				//console.log($scope.properties);
+			});
+		};
+		
+		var businesslist = function(){
+			dataService.get("/getmultiple/business/"+$scope.bizListCurrentPage+"/"+$scope.pageItems)
+			.then(function(response){
+				$scope.totalRecords=response.totalRecords;
+				$scope.bizList=response.data;
+				console.log(response.data);
+			});	
+			//This code for publish unpublish button{sonali}
 			
-			 $scope.verify = function(id, verified){
+			$scope.verify = function(id, verified){
 				$scope.veryfiedData = {verified : verified};
 				console.log($scope.veryfiedData);
 				dataService.put("put/business/"+id, $scope.veryfiedData)
 				.then(function(response) { //function for businesslist response
 					console.log(response);
 				});
-			} 
-			
-		//This code for featured unfeatured button {sonali}
-			 $scope.feature = function(id, featured){
+			} ;
+			//This code for featured unfeatured button {sonali}
+			$scope.feature = function(id, featured){
 				$scope.featuredData = {featured : featured};
 				console.log($scope.featuredData);
 				dataService.put("put/business/"+id, $scope.featuredData)
 				.then(function(response) { //function for businesslist response
 					console.log(response);
 				});
-			} 
-			
+			};
+			$scope.radioModel = 'Middle';
+
+				$scope.checkModel = {
+				left: false,
+				//middle: true,
+				right: true
+			};	
+				
 			if($routeParams.id){
 			//Update business
 				dataService.get("getsingle/business/"+$routeParams.id)
@@ -54,16 +103,29 @@ define(['app'], function (app) {
 						alert(response);
 					});
 				};
-			}
+			}		
+			
+		};
 		
-			$scope.radioModel = 'Middle';
-
-			  $scope.checkModel = {
-				left: false,
-				//middle: true,
-				right: true
-			  };
-		//end of code
+		var deletedbusiness = function(){
+			dataService.get("/getmultiple/business/"+$scope.delBizCurrentPage+"/"+$scope.pageItems)
+			.then(function(response){
+				$scope.totalRecords=response.totalRecords;
+				$scope.delBiz=response.data;
+				console.log(response.data);
+			});		
+		};
+		
+		//method for insert data of add business form{trupti}
+		$scope.insert = function(addbusiness){
+			console.log($scope.addbusiness);
+			dataService.post("../server-api/index.php/post/business",$scope.addbusiness)
+			.then(function(response) {
+				alert(response);
+				console.log(response);
+			})
+		}	//end of insert
+	
 		
 		// This code for Date Picker {Vilas}
 		$scope.today = function(){
@@ -85,64 +147,18 @@ define(['app'], function (app) {
 		$scope.format = $scope.formats[0];
 		/* Date Picker Ended here --------------------------------------------------------------------------------------*/
 		
-		// This will change businessView dynamically from 'business.html' {Vilas}
-		
-		$scope.businessView = $routeParams.businessView;
-		console.log($scope.businessView );
-		
-		//for display default businesslist.html{trupti}
-		if(!$routeParams.businessView) {
-			$location.path('/dashboard/business/businesslist');
-		}
-		
-		// Add Business multi part form show/hide operation from here! {Vilas}
-		$scope.formPart = 'home';
-		console.log($scope.formPart);
-		$scope.showFormPart = function(formPart){
-			$scope.formPart = formPart;
-		};  
-
-
-		//Code For Pagination
-		$scope.maxSize = 5;
-		$scope.totalRecords = "";
-		$scope.bizListCurrentPage = 1;
-		$scope.delBizCurrentPage = 1;
-		$scope.pageItems = 10;
-		$scope.numPages = "";		
-
-		$scope.pageChanged = function(page) {
-			//$log.log('Page changed to: ' + $scope.currentPage);
-			//get request for businesslist
-			dataService.get("/getmultiple/business/"+page+"/"+$scope.pageItems)
-			.then(function(response) { //function for businesslist response
-				$scope.bizList = response.data;
-				//$scope.totalRecords = response.totalRecords;
-				//console.log($scope.properties);
-			});
-			//get request for delete bizlist 
-			dataService.get("/getmultiple/business/"+page+"/"+$scope.pageItems)
-			.then(function(response) { //function for deltebiz response
-				$scope.delBiz = response.data;
-				//$scope.totalRecords = response.totalRecords;
-				//console.log($scope.properties);
-			});
+		switch($scope.formPart) {
+			case 'businesslist':
+				businesslist();
+				break;
+			case 'deletedbusiness':
+				deletedbusiness();
+				break;
+			default:
+				businesslist();
 		};
 		
-		dataService.get("/getmultiple/business/"+$scope.bizListCurrentPage+"/"+$scope.pageItems)
-		.then(function(response){
-			$scope.totalRecords=response.totalRecords;
-			$scope.bizList=response.data;
-			console.log(response.data);
-		});
-		
-		dataService.get("/getmultiple/business/"+$scope.delBizCurrentPage+"/"+$scope.pageItems)
-		.then(function(response){
-			$scope.totalRecords=response.totalRecords;
-			$scope.delBiz=response.data;
-			console.log(response.data);
-		});
-    }
+    };
 	
 	// Inject controller's dependencies
 	businessController.$inject = injectParams;
