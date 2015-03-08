32a0009e69b89e1c6b0b65c92a78c01c6a75a59b
Aarti Kulkarni <aarti@wtouch.in>
2015-03-08 17:27:23 +0530

    add swicth case to enquiryController.js for performing multiple functions

--- app/modules/enquiry/enquiryController.js
+++ app/modules/enquiry/enquiryController.js
@@ -5,49 +5,89 @@ define(['app'], function (app) {
     // This is controller for this view
 	var enquiryController = function ($scope, $injector, $routeParams,$location,dataService) {
 		
-		//$scope.MailView = $routeParams.mailId; /* this object will check list of mails show or single mail show */
-		$scope.mailId = $routeParams.mailId; 
-		console.log($scope.mailId);
-		
-		//For display by default mails.html page{trupti}
-		if(!$routeParams.mailId) {
-		$location.path('/dashboard/enquiry/mails');
-		}
-		
 		//Code For Pagination
 		$scope.maxSize = 5;
 		$scope.totalRecords = "";
 		$scope.mailListCurrentPage = 1;
 		$scope.sentmailListCurrentPage = 1;
+		$scope.delmailListCurrentPage = 1;
 		$scope.pageItems = 10;
-		$scope.numPages = "";		
+		$scope.numPages = "";
+		//$scope.status = {status : 3};
+		$scope.user_id = {user_id : 2};
+		// this object will check list of mails show or single mail show 
+		$scope.mailId = $routeParams.mailId; 
+		
+		//For display by default mails.html page{trupti}
+		if(!$routeParams.mailId) {
+			$location.path('/dashboard/enquiry/mails');
+		}
+		
 		$scope.pageChanged = function(page) { 
-			//$log.log('Page changed to: ' + $scope.currentPage);
-			//get request for templatelist 
-			/*$http.get("../server-api/index.php/properties/"+$scope.mailListCurrentPage+"/"+$scope.pageItems)
-			.success(function(response) {  //function for maillatelist response
-				$scope.mails.mailListCurrentPage = response.mails.mailListCurrentPage;
-				//$scope.totalRecords = response.totalRecords;
-				//console.log($scope.properties);*/
-				dataService.get("getmultiple/enquiry/"+page+"/"+$scope.pageItems).then(function(response){
-					$scope.mailList = response.data;
-					console.log(response.data);
-				});
-				
-				dataService.get("getmultiple/enquiry/"+page+"/"+$scope.pageItems).then(function(response){
-					$scope.sentmailList = response.data;
-					console.log(response.data);
-				});
+			dataService.get("getmultiple/enquiry/"+page+"/"+$scope.pageItems).then(function(response){
+				$scope.mailList = response.data;
+				//console.log(response.data);
+			});
 		};
 		//End of pagination
-	
-		dataService.get("getmultiple/enquiry/"+$scope.mailListCurrentPage+"/"+$scope.pageItems)
-		.then(function(response) {  
-			$scope.mailList = response.data;
-			$scope.totalRecords = response.totalRecords;
-			console.log(response.data);
-		});
-	 };
+		
+		var inboxmailList = function(){
+			dataService.get("getmultiple/enquiry/"+$scope.mailListCurrentPage+"/"+$scope.pageItems)
+			.then(function(response) {  
+				$scope.mailList = response.data;
+				$scope.totalRecords = response.totalRecords;
+			});
+		}
+		
+		var sentmailList = function(){
+			dataService.get("getmultiple/enquiry/"+$scope.sentmailListCurrentPage+"/"+$scope.pageItems).then(function(response){
+				$scope.mailList = response.data;
+				console.log(response.data);
+			});
+		}
+		
+		var deletemailList = function(){
+			dataService.get("getmultiple/enquiry/"+$scope.delmailListCurrentPage+"/"+$scope.pageItems).then(function(response){
+				$scope.mailList = response.data;
+				console.log(response.data);
+			});
+		}
+		
+		var composeMail = function(){
+			//reset function{trupti}
+			$scope.reset = function() {
+				$scope.compose = {};
+			};
+			//post method for insert data in request template form{trupti}
+			$scope.postData = function(reqtemp) { 
+				dataService.post("/post/template",reqtemp)
+				.then(function(response) {  //function for response of request temp
+					$scope.reqtemp = response.data;
+					console.log(response);
+					$scope.reset();
+				});
+				
+			}//end of post method{trupti}
+		}
+		
+		switch($scope.mailId) {
+			case 'inboxmailList':
+				inboxmailList();
+				break;
+				
+			case 'sentmailList':
+				sentmailList();
+				break;
+				
+			case 'deletemailList':
+				deletemailList();
+				break;
+				
+			default:
+				inboxmailList();
+		};
+		
+	};
 
 	// Inject controller's dependencies
 	enquiryController.$inject = injectParams;
@@ -55,3 +95,10 @@ define(['app'], function (app) {
     app.register.controller('enquiryController', enquiryController);
 	
 });
+//$log.log('Page changed to: ' + $scope.currentPage);
+			//get request for templatelist 
+			/*$http.get("../server-api/index.php/properties/"+$scope.mailListCurrentPage+"/"+$scope.pageItems)
+			.success(function(response) {  //function for maillatelist response
+				$scope.mails.mailListCurrentPage = response.mails.mailListCurrentPage;
+				//$scope.totalRecords = response.totalRecords;
+				//console.log($scope.properties);*/
