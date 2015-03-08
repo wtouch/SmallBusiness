4a45d4d0f410c10a14158ada1c64f4c01f006fb4
Trupti Misal <trupti@wtouch.in>
2015-03-08 12:33:41 +0530

    listoftemplates.html
    requestcustomtemplates.html
    templatesController.js edited

--- app/modules/templates/templatesController.js
+++ app/modules/templates/templatesController.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 define(['app'], function (app) {
@@ -15,15 +14,21 @@ define(['app'], function (app) {
 			$location.path('/dashboard/templates/listoftemplates');
 		}
 			
+		//reset function{trupti}
+		$scope.reset = function() {
+		$scope.reqtemp = {};
+		};		
+			
 		//post method for insert data in request template form{trupti}
-		$scope.postData = function() { 
-			dataService.post("/post/template",$scope.reqtemp)
+		$scope.postData = function(reqtemp) { 
+			dataService.post("/post/template",reqtemp)
 			.then(function(response) {  //function for response of request temp
-				$scope.reqtemp = response.reqtemp;
-				console.log(response.reqtemp);
+				$scope.reqtemp = response.data;
+				console.log(response);
+				$scope.reset();
 			});
-		}
-		//end of post method
+			
+		}//end of post method{trupti}
 	
 		//Code For Pagination{trupti}
 		$scope.maxSize = 5;
@@ -41,20 +46,42 @@ define(['app'], function (app) {
 				$scope.templates = response.data;
 				//console.log($scope.properties);
 			});
-			
+			//this is conditional if for open my template list{trupti}
+			if($routeParams.tempPart=='mytemplates') {
 			//get request for  mytemplate{trupti}
 			dataService.get("/getmultiple/template/"+$scope.myTempCurrentPage+"/"+$scope.pageItems)
 			.then(function(response) {  //function for my templates response
 				$scope.templates = response.data;
 				//console.log($scope.properties);
 			});
-			
+			}//end conditional if{trupti}
+			else{
+				//for open by default templist{trupti}
+				dataService.get("/getmultiple/template/"+$scope.tempListCurrentPage+"/"+$scope.pageItems)
+				.then(function(response) {  //function for templatelist response
+				$scope.totalRecords = response.totalRecords;
+				$scope.templates = response.data;
+				console.log(response.data);
+			});//end of by default templist
+			}//end else
+				
+			//this is conditional if for open custom template list{trupti}
+			if($routeParams.tempPart=='custometemplates') {
 			//get request for  custom template{trupti}
 			dataService.get("/getmultiple/template/"+$scope.customTempCurrentPage+"/"+$scope.pageItems)
 			.then(function(response) {  //function for custom templates response
 				$scope.templates = response.data;
 			}); 
-			
+			}//end if
+			else{
+				//for open by default templist{trupti}
+				dataService.get("/getmultiple/template/"+$scope.tempListCurrentPage+"/"+$scope.pageItems)
+				.then(function(response) {  //function for templatelist response
+				$scope.totalRecords = response.totalRecords;
+				$scope.templates = response.data;
+				console.log(response.data);
+			});//end of by default templist
+			}//end else
 		};//End of pagination
 		dataService.get("/getmultiple/template/"+$scope.tempListCurrentPage+"/"+$scope.pageItems)
 		.then(function(response) {  //function for templatelist response
