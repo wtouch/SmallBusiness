d8db0f85a3b53248db5b7a971e4f9d4278f3dfe4
Sonali Shinde <sonali@wtouch.in>
2015-03-12 11:47:24 +0530

    add image upload in businessprofile.html

--- app/modules/business/addbusiness/addbusinessController.js
+++ app/modules/business/addbusiness/addbusinessController.js
@@ -1,11 +1,22 @@
 'use strict';
 
 define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector','$routeParams','$location','dataService','modalService'];
+    var injectParams = ['$scope', '$injector','$routeParams','$location','dataService','upload','modalService'];
 
     // This is controller for this view
-	var addbusinessController = function ($scope, $injector,$routeParams,$location,dataService,modalService)
+	var addbusinessController = function ($scope, $injector,$routeParams,$location,dataService,upload,modalService)
 	{
+		
+		//for display form parts
+		$scope.formPart = $routeParams.formPart;
+		// all $scope object goes here
+		$scope.alerts = [];
+		$scope.maxSize = 5;
+		$scope.totalRecords = "";
+		$scope.pageItems = 10;
+		$scope.numPages = "";
+		$scope.user_id = {user_id : 2};
+		
 		// Add Business multi part form show/hide operation from here! {Vilas}
 		$scope.formPart = 'home';
 		console.log($scope.formPart);
@@ -13,7 +24,28 @@ define(['app'], function (app) {
 			$scope.formPart = formPart;
 		};  
 		
-		//method for insert data of add business form{trupti}
+		
+		//Upload Function for uploading files {Vilas}
+		$scope.businessprofile={}; // this is form object
+		$scope.userinfo = {userId:1, name:"vilas"}; // this is for uploading credentials
+		$scope.path = "business/"; // path to store images on server
+		$scope.businessprofile.scrible  = []; // uploaded images will store in this array
+		$scope.upload = function(files,path,userinfo){ // this function for uploading files
+			upload.upload(files,path,userinfo,function(data){
+				if(data.status === 'success'){
+					$scope.businessprofile.scrible.push(JSON.stringify(data.details));
+					console.log(data.message);
+				}else{
+					$scope.alerts.push({type: response.status, msg: response.message});
+				}
+				
+			});
+		};
+		$scope.generateThumb = function(files){  // this function will generate thumbnails of images
+			upload.generateThumbs(files);
+		};// End upload function
+		
+		//method for insert data of add businessProfile form{trupti}
 		$scope.insert = function(addbusiness){
 			console.log($scope.addbusiness);
 			dataService.post("../server-api/index.php/post/business",$scope.addbusiness)
