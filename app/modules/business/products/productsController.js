803e0d1fa575fb1a0ab2ef295f35eaa8f89f0235
Trupti Misal <trupti@wtouch.in>
2015-03-15 14:15:37 +0530

    addproducts.html edited
    products.html
    productsController.js
    edited

--- app/modules/business/products/productsController.js
+++ app/modules/business/products/productsController.js
@@ -17,7 +17,7 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 		$scope.addproductCurrentPage = 1;
 		$scope.pageItems = 10;
 		$scope.numPages = "";
-		$scope.user_id = {user_id : 2};
+		$scope.userDetails = {user_id : 2};
 		//$scope.selectBusiness = {};
 		
 		//Upload Function for uploading files {Vilas}
@@ -27,7 +27,7 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 		$scope.path = "product/"; // path to store images on server
 		$scope.addproduct.product_image  = []; // uploaded images will store in this array
 		$scope.addservice.product_image  = []; // uploaded images will store in this array
-		$scope.upload = function(files,path,userinfo,imgArr){ // this function for uploading files
+		$scope.upload = function(files,path,userinfo,imgArr){//this function for uploading files
 		console.log(imgArr);
 			upload.upload(files,path,userinfo,function(data){
 				if(data.status === 'success'){
@@ -43,57 +43,70 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 			upload.generateThumbs(files);
 		};// End upload function
 		
-		
-		
 		$scope.changeScope = function(value, object){
 			$scope.addservice.business_id = value.id;
 			$scope.addproduct.business_id = value.id;
 			console.log($scope.addproduct);
 		}
-		
+		$scope.addProduct = false;
+		$scope.addService = false;
+		$scope.showForm = function(object){
+			console.log(object);
+			$scope[object] = ($scope[object]==true) ? false : true;
+			$scope.$apply;
+		}
 		// get data for business options 
-		dataService.get("/getmultiple/business/1/100")
+		
+		dataService.get("getmultiple/business/1/100",$scope.userDetails)
 			.then(function(response) {  //function for template list response
+			//$scope.businessList.user_id=$scope.userDetails.user_id;
 				if(response.status == 'success'){
 					$scope.businessList = response.data;
 					console.log($scope.businessList);
 				}else{
 					
+					$scope.alerts.push({type: response.status, msg: "You didn't added any business! Please add business first."});
 				}
+				$scope.businessList = response.data;
+				//console.log(businessList);
 			});
 		//end of options
 		
+		
 		var addproducts = function(){
 			//reset function{trupti}
-			
-			
+			//angular.extend($scope.addproducts,$scope.userDetails)
 			//console.log($scope.addproduct.business_id);
 			$scope.reset = function() {
 				$scope.addproduct = {};
 			};
 			$scope.postData = function(addproduct) { 
-			addproduct.user_id=$scope.user_id.user_id;
-			
-			//console.log(user_id);
+			addproduct.user_id=$scope.userDetails.user_id;
+			////console.log(user_id);
 				dataService.post("post/product",addproduct)
 				.then(function(response) {  //function for response of request temp
 					$scope.addproduct = response.data;
 					console.log(response);
 					$scope.reset();
 				});
+				
+				console.log(addproduct);
 			}//end of post method {trupti}
+			
+			//method for close{trupti}
+			$scope.ok = function () {
+			$addservice.close('ok');
+		};
 		}
 		
 		var addservices = function(){
 			//reset function{trupti}
-			
-			
 			//console.log($scope.addproduct.business_id);
 			$scope.reset = function() {
 				$scope.addservice = {};
 			};
 			$scope.postData = function(addservice) { 
-			addproduct.user_id=$scope.user_id.user_id;
+			addservice.user_id=$scope.userDetails.user_id;
 			
 			//console.log(user_id);
 				dataService.post("post/product",addservice)
@@ -122,6 +135,5 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 	productsController.$inject = injectParams;
 	// Register/apply controller dynamically
     app.register.controller('productsController', productsController);
-	
 	
 });
