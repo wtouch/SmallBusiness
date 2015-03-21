11b9ea0be808c4b9bbd9dd721120c9fb237b4fc1
Pooja Nikam <pooja@wtouch.n>
2015-03-21 19:56:23 +0530

    create template

--- server-api/templates/default/index.php
+++ server-api/templates/default/index.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title><?php echo $userShortInfo ?></title>
-</head>
-<body>
-<nav>navigation</nav>
-<?php if(isset($_GET['view'])){
-	include $_GET['view'].".php";
-} ?>
-	<p></p>
-<footer>footer</footer>
-</body>
+	<head>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<title>Welcome </title>
+	</head>
+	<body>
+		<?php include ('header.html');
+			  include ('navbar.html');
+			  include ('home.html');
+		?>
+		
+		<?php if(isset($_GET['view'])){			
+			include $_GET['view'].".php";
+			}
+		?>
+			<p></p>
+			
+		<?php 
+			  include ('footer.html');
+		?>
+		
+
+	</body>
 </html>
