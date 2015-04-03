409e31c8b6fe7fede9d788ffc411c695e33df45d
vilas <vilas@wtouch.in>
2015-04-03 22:20:48 +0530

    templateLoader modified into class.

--- website/index.php
+++ website/index.php
@@ -11,38 +11,50 @@ $app = new Slim\Slim();
 $loader = new Twig_Loader_Filesystem($config['root_path'].'/website/templates');
 // initialize Twig environment
 $twig = new Twig_Environment($loader);
+$web = new websiteManager;
+$routes = $web->getRoutes();
 
-
-//$app->response->headers->set('Content-Type', 'application/json');
-
-if(isset($_GET['_escaped_fragment_'])){
-	$seo = true;
-}else{
-	$seo = false;
+foreach($routes['data'] as $route){
+	$app->get('/'.$route, function() use($app, $web, $twig, $config, $route) {
+		$template = $twig->loadTemplate('child.tmpl');
+		$webConfig = $web->getConfig();
+		$webData = $web->getData();
+		$webRoutes = $web->getRoutes();
+		$webTemplate = $web->getTemplate();
+		$response['path'] = $config['http_template_path'];
+		if($webConfig['status'] == 'success'){
+			$response['webConfig'] = $webConfig['data'];
+		}else{
+			throw new Exception($webConfig['message']);
+		}
+		if($webData['status'] == 'success'){
+			$response['webData'] = $webData['data'];
+		}else{
+			throw new Exception($webData['message']);
+		}
+		if($webRoutes['status'] == 'success'){
+			$response['webRoutes'] = $webRoutes['data'];
+		}else{
+			throw new Exception($webRoutes['message']);
+		}
+		if($webTemplate['status'] == 'success'){
+			$response['webTemplate'] = $webTemplate['data'];
+		}else{
+			throw new Exception($webTemplate['message']);
+		}
+		$template->display($response);
+		//loadTemplate($seo);
+	});
 }
-$app->get('/', function() use($app, $seo, $twig,$config) {
-	//echo $seo;
-	// load template
-	$web = new websiteManager;
-	print_r($web->getRoutes());
-	$template = $twig->loadTemplate('child.tmpl');
-	$template->display(array(
-	'path' => $config['http_template_path'],
-	'title' => 'Small Business',
-    'name' => 'Clark Kent',
-    'username' => 'ckent',
-    'password' => 'krypt0n1te',
-  ));
-	//loadTemplate($seo);
-});
 
 $app->get('/sitedata', function() use($app, $config) {
 	try{
 		$app->response->headers->set('Content-Type', 'application/json');
-		$webConfig = websiteManager::getConfig();
-		$webData = websiteManager::getData();
-		$webRoutes = websiteManager::getRoutes();
-		$webTemplate = websiteManager::getTemplate();
+		$web = new websiteManager;
+		$webConfig = $web->getConfig();
+		$webData = $web->getData();
+		$webRoutes = $web->getRoutes();
+		$webTemplate = $web->getTemplate();
 		if($webConfig['status'] == 'success'){
 			$response['webConfig'] = $webConfig['data'];
 		}else{
