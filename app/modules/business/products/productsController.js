c273b574e41745b9ede8cfc965164791ce97ea30
Dnyaneshwar Shetkar <dnyaneshwar@wtouch.in>
2015-04-04 19:42:16 +0530

    changes in product form and list.

--- app/modules/business/products/productsController.js
+++ app/modules/business/products/productsController.js
@@ -5,8 +5,6 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 
     // This is controller for this view
 	var productsController = function ($scope,$rootScope,$injector,$location,$routeParams,dataService,upload,modalService) {
-		//console.log("Product Controller"); // Just Added for testing {Vilas}
-		
 		//for display form parts of product & service
 		$scope.productView = $routeParams.productView;
 	
@@ -19,27 +17,27 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 		$scope.numPages = "";
 		$scope.userDetails = {user_id : $rootScope.userDetails.id};
 		$scope.currentDate = dataService.currentDate;
-		// for testing
-		
+		$scope.path = "product/"; // path to store images on server
+		$scope.showProductForm = false;
+		$scope.showServiceForm = false;
+		$scope.editProductForm = false;
+		$scope.productType = "service";
 		//Upload Function for uploading files {Vilas}
 		$scope.addproduct = {
 			product_image : {}
-		}; // this is form object
+		}; 
+		// this is form object
 		$scope.addservice = {
 			product_image : {}
 		};
 		$scope.userinfo = {user_id:1}; // this is for uploading credentials
-		
-		$scope.path = "product/"; // path to store images on server
 		$scope.upload = function(files,path,userinfo,picArr){//this function for uploading files
-		console.log(picArr);
 	
 			upload.upload(files,path,userinfo,function(data){
 				var picArrKey = 0, x;
 				for(x in picArr) picArrKey++;
 				if(data.status === 'success'){
 					picArr[picArrKey] = data.data;
-					console.log(data.message);
 				}else{
 					$scope.alerts.push({type: data.status, msg: d.message});
 				}
@@ -50,22 +48,11 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 		$scope.generateThumb = function(files){  // this function will generate thumbnails of images
 			upload.generateThumbs(files);
 		};// End upload function
-		
-		//for change image src dynamically
-		$scope.imgkey = '0';
-		$scope.changeSrc = function(key){
-			$scope.imgkey = key;
-		}
-		
 		$scope.changeScope = function(value, object){
 			$scope.addservice.business_id = value.id;
 			$scope.addproduct.business_id = value.id;
 			$scope.changeScopeObject($scope.productType);
 		};
-		
-		//$scope.selectBusiness = {id : 2};
-		$scope.showProductForm = false;
-		$scope.showServiceForm = false;
 		$scope.showForm = function(object){
 			$scope[object] = ($scope[object]==true) ? false : true;
 			$scope.$apply;
@@ -75,8 +62,14 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 			if(object == 'showServiceForm'){
 				addservices();
 			}
-			
+			$scope.editProductForm = false;//this is for closing the edit form
 		}
+		$scope.editForm = function(x1){
+			$scope.showServiceForm = true;
+			$scope.editProductForm = true;
+			angular.extend($scope.addservice, x1);
+		}
+		
 		// get data for business options 
 		
 		//for close alert
@@ -100,7 +93,6 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 		//end of options
 		var addproducts = function(){ 
 			//reset function{trupti}
-				console.log($scope.addproduct.business_id);
 				$scope.reset = function(){
 				$scope.addproduct = {};
 			};
@@ -113,46 +105,61 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 				 dataService.post("post/product",addproduct)
 				.then(function(response) {  //function for response of request temp
 					$scope.addproduct = response.data;
-					console.log(response.message);
 					$scope.reset();
-				}); 
-				console.log(addproduct);
-			}//end of post method {trupti}  
-			console.log("add product");
+				});
+			}//end of post method {trupti} 
 		}
 		
 		var addservices = function(){
 			
 		    $scope.addservice.user_id= $rootScope.userDetails.id;
-			$scope.postData = function(addservice) { 
-				console.log(addservice);
+			$scope.postData = function(addservice) {
 				$scope.userDetails=$scope.userDetails;
 				$scope.addservice.date = $scope.currentDate;
 				dataService.post("post/product",addservice)
 				.then(function(response) {  //function for response of request temp
-					console.log(response);
 				});
 			} //end of post method {trupti} 
 		}
-	
 		//view for product list
 		var productlist= function(){
-				$scope.productFilter = {business_id : $scope.selectBusiness.id, type : 'product'};
-				angular.extend($scope.userDetails, $scope.productFilter);
-				dataService.get("getmultiple/product/1/10",$scope.userDetails)
-				.then(function(response) {  
-					if(response.status == 'success'){
-						$scope.products = dataService.parse(response.data);
-					
-						console.log($scope.products);
-						console.log(response.data);
-					}else{
-						$scope.alerts.push({type: response.status, msg: response.message});
-						$scope.products = [];
-					};
-				});
+					$scope.productFilter = {business_id : $scope.selectBusiness.id, type : 'product'};
+					angular.extend($scope.userDetails, $scope.productFilter);
+					dataService.get("getmultiple/product/1/10",$scope.userDetails)
+					.then(function(response) {  
+						if(response.status == 'success'){
+							$scope.products = dataService.parse(response.data);
+						}else{
+							$scope.alerts.push({type: response.status, msg: response.message});
+							$scope.products = [];
+						};
+					});
 				
-			}	
+			}
+			$scope.dynamicTooltip = function(status, active, notActive){
+						return (status==1) ? active : notActive;
+					};
+						
+		//This code for featured unfeatured button {sonali}
+			$scope.feature = function(id, featured){
+					$scope.featuredData = {featured : featured};
+					dataService.put("put/product/"+id, $scope.featuredData)
+					.then(function(response) { //function for businesslist response
+						$scope.alerts.push({type: response.status, msg: response.message});
+					});
+			};
+		//This code for featured unfeatured button {sonali}
+			$scope.deleted = function(id, status){
+				$scope.deletedData = {status : status};
+				//console.log($scope.deletedData);
+				dataService.put("put/product/"+id, $scope.deletedData)
+				.then(function(response) { //function for businesslist response
+					if(response.status == 'success'){
+						$scope.hideDeleted = 0;
+					}
+					$scope.alerts.push({type: response.status, msg: response.message});
+				});
+			};
 
 		var servicelist= function(){
 			$scope.productFilter = {business_id : $scope.selectBusiness.id, type : 'service'};
@@ -161,7 +168,6 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 				.then(function(response) {  
 					if(response.status == 'success'){
 						$scope.services = dataService.parse(response.data);
-						console.log($scope.services);
 					}
 					else
 					{
@@ -170,18 +176,14 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 					};
 				});
 		}
-		
-		$scope.productType = "service";
 		$scope.changeScopeObject = function(value){
 			$scope.productType = value;
-			console.log($scope.showProductForm);
 			if(value=="product" && $scope.showProductForm == true){
 				addproducts();
 			}
 			
 			if(value=="product"){
 				productlist();
-				console.log("product if");
 			}
 			
 			if(value=="service" && $scope.showServiceForm == true){
@@ -190,7 +192,6 @@ define(['app','css!modules/business/products/products.css'], function (app) {
 			
 			if(value=="service"){
 				servicelist();
-				console.log("service if");
 			}
 		}
 	
