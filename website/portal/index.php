4ad5f7182b9a6edb33745e04fe6249b856fac4a2
vilas <vilas@wtouch.in>
2015-04-07 19:17:59 +0530

    portalManager added.

--- website/portal/index.php
+++ website/portal/index.php
@@ -3,16 +3,16 @@
 require $config['root_path'].'/server-api/lib/Slim/Slim.php';
 require $config['root_path'].'/server-api/lib/Twig/Autoloader.php';
 require $config['root_path'].'/server-api/modules/db/dbHelper.php';
+require 'models/portalManager.php';
 
 Slim\Slim::registerAutoloader();
 Twig_Autoloader::register();
 $app = new Slim\Slim();
 $db = new dbHelper;
+$portal = new portalManager;
 
-$app->get('/', function() use($app, $config) {
-	
-	echo json_encode($config);
-	
+$app->get('/', function() use($app, $config, $db,$portal) {
+	echo $portal->getBusinessData();
 });
 
 $app->run();
