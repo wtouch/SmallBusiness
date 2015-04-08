4105877c97114c0a601432dc83a9a32a2d2ddcc4
vilas <vilas@wtouch.in>
2015-04-08 10:10:34 +0530

    Google map modified with callback function.

--- app/modules/websites/websettings/websettingsController.js
+++ app/modules/websites/websettings/websettingsController.js
@@ -6,99 +6,97 @@ define(['app'], function (app) {
     // This is controller for this view
 	var websettingsController = function ($scope,$rootScope,$injector,$routeParams,$location,dataService,upload,modalService) {
 	// all $scope object goes here
-	$scope.userDetails = {user_id : $rootScope.userDetails.id};
-	$scope.website_id=$routeParams.id;
-	$scope.currentDate = dataService.currentDate;
-	$scope.alerts = [];
-	//this is for get business name from business
-	dataService.get("getmultiple/business/1/100",$scope.userDetails)
-			.then(function(response) {  //function for template list response
-			//$scope.businessList.user_id=$scope.userDetails.user_id;
-				if(response.status == 'success'){
-					$scope.businessList = response.data;
-					
-				}else{
-					
-					$scope.alerts.push({type: response.status, msg: "You didn't added any business! Please add business first."});
-				}
-			});
-			
-			//this is for get business name from business{trupti}
-			dataService.get("getmultiple/mytemplate/1/100",$scope.userDetails)
-			.then(function(response) {  //function for template list response
-			//$scope.templateList.user_id=$scope.userDetails.user_id;
-				if(response.status == 'success'){
-					$scope.templateList = response.data;
-					
-				}
-				else{
-					
-					$scope.alerts.push({type: response.status, msg: "You didn't added any business! Please add business first."});
-				}
-			});
-			
-			//code for edit website details{trupti}
-			$scope.userInfo = dataService.parse($rootScope.userDetails);
-				dataService.get("getsingle/website/"+$routeParams.id,$scope.userDetails)
-				.then(function(response) {  //function for my templates response
-				if(response.status == 'success'){
-					if(response.data.config!=''){
+		$scope.userDetails = {user_id : $rootScope.userDetails.id};
+		$scope.website_id=$routeParams.id;
+		$scope.currentDate = dataService.currentDate;
+		$scope.alerts = [];
+		//this is for get business name from business
+		dataService.get("getmultiple/business/1/100",$scope.userDetails)
+		.then(function(response) {  //function for template list response
+		//$scope.businessList.user_id=$scope.userDetails.user_id;
+			if(response.status == 'success'){
+				$scope.businessList = response.data;
+				
+			}else{
+				
+				$scope.alerts.push({type: response.status, msg: "You didn't added any business! Please add business first."});
+			}
+		});
+		
+		//this is for get business name from business{trupti}
+		dataService.get("getmultiple/mytemplate/1/100",$scope.userDetails)
+		.then(function(response) {  //function for template list response
+			if(response.status == 'success'){
+				$scope.templateList = response.data;
+			}else{
+				$scope.alerts.push({type: response.status, msg: "You don't have any template! Please get free template or buy template first."});
+			}
+		});
+		
+		//code for edit website details{trupti}
+		$scope.userInfo = dataService.parse($rootScope.userDetails);
+		dataService.get("getsingle/website/"+$routeParams.id,$scope.userDetails)
+		.then(function(response) {  //function for my templates response
+			if(response.status == 'success'){
+				if(response.data.config!=''){
 					var config = JSON.parse(response.data.config);
 					$scope.config = config;
 					console.log(config);
-					}
-					
-				}
-				else
-				{
-					$scope.alerts.push({type: response.status, msg: response.message});
-				};
-				//$scope.config = response.data;
-			});
-			//update method for website settings form{trupti}
-			$scope.editWebsitedetails = function(config){
-				dataService.put("put/website/"+$routeParams.id,{config : config})
-				.then(function(response) {
-					if(response.status == 'success'){
-						$scope.alerts.push({type: response.status, msg: response.message});
+					if(config.google_map.latitude && config.google_map.longitude){
+						$scope.initGoogleMap(config.google_map.latitude, config.google_map.longitude, 18);
 					}else{
-						$scope.alerts.push({type: (response.status == 'error') ? "danger" :response.status, msg: response.message});
+						$scope.getLocation();
 					}
-				}) 
-			}
-			
-			
-		// Google Map
-		
-		$scope.map = {
-			"center": {
-				"latitude": 18.520430299999997,
-				"longitude": 73.8567437
-			},
-			"zoom": 5
-		}; //TODO:  set location based on users current gps location 
-		 $scope.marker = {
-			id: 0,
-			coords: {
-				latitude: 18.520430299999997,
-				longitude: 73.8567437
-			},
-			options: { draggable: true },
-			events: {
-				dragend: function (marker, eventName, args) {
-					$scope.config.google_map.latitude = $scope.marker.coords.latitude;
-					$scope.config.google_map.longitude = $scope.marker.coords.longitude;
-					console.log($scope.config.google_map.latitude);
-					console.log($scope.marker.coords);
-					$scope.marker.options = {
-						draggable: true,
-						labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-						labelAnchor: "100 0",
-						labelClass: "marker-labels"
-					};
 				}
-			}
-		};
+			}else{
+				$scope.alerts.push({type: response.status, msg: response.message});
+			};
+			//$scope.config = response.data;
+		});
+		//update method for website settings form{trupti}
+		$scope.editWebsitedetails = function(config){
+			dataService.put("put/website/"+$routeParams.id,{config : config})
+			.then(function(response) {
+				if(response.status == 'success'){
+					$scope.alerts.push({type: response.status, msg: response.message});
+				}else{
+					$scope.alerts.push({type: (response.status == 'error') ? "danger" :response.status, msg: response.message});
+				}
+			}) 
+		}
+		// Google Map
+		$scope.initGoogleMap = function(latitude,longitude, zoom){
+			$scope.config.google_map.latitude = latitude;
+			$scope.config.google_map.longitude = longitude;
+			$scope.map = {
+				"center": {
+					"latitude": latitude,
+					"longitude": longitude
+				},
+				"zoom": zoom
+			}; //TODO:  set location based on users current gps location 
+			$scope.marker = {
+				id: 0,
+				coords: {
+					latitude: latitude,
+					longitude: longitude
+				},
+				options: { draggable: true },
+				events: {
+					dragend: function (marker, eventName, args) {
+						$scope.config.google_map.latitude = $scope.marker.coords.latitude;
+						$scope.config.google_map.longitude = $scope.marker.coords.longitude;
+						console.log($scope.config.google_map);
+						$scope.marker.options = {
+							draggable: true,
+							labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+							labelAnchor: "100 0",
+							labelClass: "marker-labels"
+						};
+					}
+				}
+			};
+		}
 		var events = {
 			places_changed: function (searchBox) {
 				var place = searchBox.getPlaces();
@@ -106,75 +104,14 @@ define(['app'], function (app) {
 					console.log('no place data :(');
 					return;
 				}
-
-				$scope.map = {
-					"center": {
-						"latitude": place[0].geometry.location.lat(),
-						"longitude": place[0].geometry.location.lng()
-					},
-					"zoom": 18
-				};
-
-				$scope.marker = {
-					id: 0,
-					coords: {
-						latitude: place[0].geometry.location.lat(),
-						longitude: place[0].geometry.location.lng()
-					},
-					options: { draggable: true },
-					events: {
-						dragend: function (marker, eventName, args) {
-							$scope.config.google_map.latitude = $scope.marker.coords.latitude;
-							$scope.config.google_map.longitude = $scope.marker.coords.longitude;
-							console.log($scope.config.google_map.latitude);
-							console.log($scope.marker.coords);
-							$scope.marker.options = {
-								draggable: true,
-								labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-								labelAnchor: "100 0",
-								labelClass: "marker-labels"
-							};
-						}
-					}
-				};
+				$scope.initGoogleMap(place[0].geometry.location.lat(), place[0].geometry.location.lng(), 15);
 			}
 		};
-
 		$scope.searchbox = { template: 'modules/websites/websettings/searchbox.html', events: events };
-
 		$scope.showPosition = function (position) {
-				$scope.config.google_map.latitude = position.coords.latitude;
-				$scope.config.google_map.longitude = position.coords.longitude;
-				$scope.map = {
-					"center": {
-						"latitude": position.coords.latitude,
-						"longitude": position.coords.longitude
-					},
-					"zoom": 10
-				};
-				$scope.marker = {
-					"id": 0,
-					"coords": {
-						"latitude": position.coords.latitude,
-						'longitude': position.coords.longitude
-					},
-					'options': { draggable: true },
-					events: {
-						dragend: function (marker, eventName, args) {
-							$scope.config.google_map.latitude = $scope.marker.coords.latitude;
-							$scope.config.google_map.longitude = $scope.marker.coords.longitude;
-							console.log($scope.marker.coords);
-							$scope.marker.options = {
-								draggable: true,
-								labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-								labelAnchor: "100 0",
-								labelClass: "marker-labels"
-							};
-						}
-					}
-				};   
+			$scope.initGoogleMap(position.coords.latitude, position.coords.longitude, 5);
+			$scope.$apply();
 		}
- 
 		$scope.showError = function (error) {
 			switch (error.code) {
 				case error.PERMISSION_DENIED:
@@ -190,9 +127,9 @@ define(['app'], function (app) {
 					$scope.error = "An unknown error occurred."
 					break;
 			}
+			console.log($scope.error);
 			$scope.$apply();
 		}
-
 		$scope.getLocation = function () {
 			if (navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
@@ -201,10 +138,6 @@ define(['app'], function (app) {
 				$scope.error = "Geolocation is not supported by this browser.";
 			}
 		}
-
-		$scope.getLocation();
-			
-			
     };
 	//Inject controller's dependencies
 	websettingsController.$inject = injectParams;
