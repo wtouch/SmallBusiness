9bacfb7864a8e06566b238a1e1aad2da7f6e6a80
Sunita Karle <sunita@wtouch.in>
2015-04-13 13:21:23 +0530

    "config.php modified"

--- Server-api/modules/db/config.php
+++ Server-api/modules/db/config.php
@@ -2,7 +2,7 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'wtoucct8_biz');
+define('DB_USERNAME', 'wtoucct8_biz');              
 define('DB_PASSWORD', 'smallbusiness');
 define('DB_HOST', 'wtouch.in');
 define('DB_NAME', 'wtoucct8_biztest');
