f4ce4a03d8414a4161f3519d2b24d06381f96c4c
vilas <vilas@wtouch.in>
2015-04-13 12:05:16 +0530

    remote db used for testing.

--- Server-api/modules/db/config.php
+++ Server-api/modules/db/config.php
@@ -2,9 +2,9 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'smallbusiness');
+define('DB_USERNAME', 'wtoucct8_biz');
+define('DB_PASSWORD', 'smallbusiness');
+define('DB_HOST', 'wtouch.in');
+define('DB_NAME', 'wtoucct8_biztest');
 
 ?>
