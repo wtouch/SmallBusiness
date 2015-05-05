3069b3ec1b21dd3f4d7f1d84e2cbd4604f8b7607
Trupti Misal <trupti@wtouch.in>
2015-05-05 13:27:23 +0530

    addproject.html
    addprojectController.js
    amenities.html
    builder.html
    domains.html
    floorplan.html
    layoutmap.html
    locationmap.html
    projectdetails.html
    projectgallery.html
    projectimage.html
    property.html edited

--- app/modules/Project/addproject/addprojectController.js
+++ app/modules/Project/addproject/addprojectController.js
@@ -1,9 +1,9 @@
 'use strict';
 define(['app'], function (app) {
 	
-	var injectParams = ['$scope','$rootScope','$injector', '$routeParams','upload','dataService','$notification'];
+	var injectParams = ['$scope','$rootScope','$injector', '$routeParams','upload','dataService'];
   // This is controller for this view
-	var addprojectController = function ($scope,$rootScope, $injector,$routeParams,upload,dataService,$notification) {
+	var addprojectController = function ($scope,$rootScope, $injector,$routeParams,upload,dataService) {
 		 $rootScope.metaTitle = "Real Estate Add Project";
 		$scope.alerts = [];
 		$scope.userinfo = {user_id : $rootScope.userDetails.id};
@@ -31,19 +31,14 @@ define(['app'], function (app) {
 			console.log(response.config_data);
 		});
 		
-		// Add Project multi part form show/hide operation from here!
-		$scope.formPart = 'project';
-		$scope.showFormPart = function(formPart){
-			$scope.formPart = formPart;
-		};
 		$scope.getData = function(location){
 			$scope.readOnly = true;
-			$scope.project.location = location.location;
-			$scope.project.city = location.city;
-			$scope.project.state = location.state;
-			$scope.project.country = location.country;
-			$scope.project.area = location.area;
-			$scope.project.pincode = location.pincode;
+			$scope.project.address.location = location.location;
+			$scope.project.address.city = location.city;
+			$scope.project.address.state = location.state;
+			$scope.project.address.country = location.country;
+			$scope.project.address.area = location.area;
+			$scope.project.address.pincode = location.pincode;
 		}
 		$scope.getTypeaheadData = function(table, searchColumn, searchValue){
 			var locationParams = {search : {}}
@@ -107,6 +102,17 @@ define(['app'], function (app) {
 			$scope.closeAlert = function(index) {
 				$scope.alerts.splice(index, 1);
 			};
+			
+			//function for Users list response
+			dataService.get("getmultiple/user/1/500", {status: 1, user_id : $rootScope.userDetails.id})
+			.then(function(response) {  
+			if(response.status == 'success'){
+				$scope.customerList = response.data;
+			}else{
+				if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+				$notification[response.status]("Get Customers", response.message);
+			}
+			});
 			 
 			// this function for uploading files
 			$scope.upload = function(files,path,userinfo, picArr){ 
@@ -114,7 +120,7 @@ define(['app'], function (app) {
 					var picArrKey = 0, x;
 					for(x in picArr) picArrKey++;
 					if(data.status === 'success'){
-						picArr[picArrKey] = data.details;
+						$scope.addlocation_map.location_image = data.data;
 						console.log(data.message);
 					}else{
 						$scope.alerts.push({type: response.status, msg: response.message});
@@ -155,8 +161,7 @@ define(['app'], function (app) {
 					$scope.alerts.push({type: response.status, msg: response.message});
 					$scope.formScope.addprojectForm.$setPristine();
 				}else{
-					if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
-				$notification[response.status]("Get Users", response.message);
+					$scope.alerts.push({type: response.status, msg: response.message});
 				}
 				
 			});
