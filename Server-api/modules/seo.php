7a8733b51def6219f6d33c5612e60fffacee819e
Vilas Shetkar <vilas@wtouch.in>
2015-06-11 20:28:02 +0530

    seo created for menus.

--- Server-api/modules/seo.php
+++ Server-api/modules/seo.php
@@ -3,55 +3,81 @@
 	$db = new dbHelper();
 	$reqMethod = $app->request->getMethod();
 	
-	/* //getMethod
+	//getMethod
 	if($reqMethod=="GET"){
-		if(isset($id)){
-			$where['id'] = $id;
-			$t0 = $db->setTable($table);
-			$db->setWhere($where, $t0);
-			$data = $db->selectSingle();
-			echo json_encode($data);
+		
+		$like = array();
+		$userId = 0;
+		$limit[0] = $pageNo;
+		$limit[1] = $records;
+		$where = array();
+		$whereProd['status'] = 1;
+		if(isset($_GET['user_id'])) $userId = $_GET['user_id'];
+		
+		(isset($_GET['status'])) ? $where['status'] = $_GET['status'] : 1;
+		
+		$userCols['name'] = "name";
+		$userCols['username'] = "username";
+		$user = $db->getUsers($userId,$userCols);
+		$db->setLimit($limit);
+		$business = $db->setJoinString("INNER JOIN", "business", array("user_id"=>$user.".id"));
+		$db->setWhere($where, $business);
+		$businessCols[0] = "*";
+		$db->setColumns($business, $businessCols);
+		
+		$product = $db->setJoinString("LEFT JOIN", "product", array("business_id"=>$business.".id"));
+		$db->setWhere($whereProd, $product);
+		$productCols = array("id" => "product_id", "product_service_name" => "product_name", "type" => "product_type", "product_image" => "product_image", "description" => "product_description", "category" => "product_category");
+		$db->setColumns($product, $productCols);
+		
+		$data = $db->select();
+		
+		$home = array("name" => "Home", "url" => "/home");
+		$about = array("name" => "About Us", "url" => "/about-us");
+		$contact = array("name" => "Contact Us", "url" => "/contact-us");
+		
+		$menus = array($home, $about, $contact);
+		
+		if($data['status'] == "success"){
+			if($data['data'][0]['testimonials']) $menus[] = array("name" => "Testimonials", "url" => "/testimonials");
+				
+			if($data['data'][0]['job_careers']) $menus[] = array("name" => "Jobs/Careers", "url" => "/job-careers");
 			
-		}else{
+			if($data['data'][0]['news_coverage']) $menus[] = array("name" => "Activities", "url" => "/activities"); 
 			
-			$like = array();
-			$userId = 0;
-			$limit[0] = $pageNo;
-			$limit[1] = $records;
-			$where = array();
-			if(isset($_GET['user_id'])) $userId = $_GET['user_id'];
+			foreach($data['data'] as $key => $value){
+				if($value['product_type'] == "product"){
+					$productCategory[$value['product_category']][] = array("name" => $value['product_name'], "url" => "/products/".$value['product_name']."/".$value['product_id']);
+				}
+				if($value['product_type'] == "service"){
+					$serviceCategory[$value['product_category']][] = array("name" => $value['product_name'], "url" => "/services/".$value['product_name']."/".$value['product_id']);
+				}
+				
+			}
 			
-			/* if(isset($_GET['search']) && $_GET['search'] == true){
-				(isset($_GET['business_name'])) ? $like['business_name'] = $_GET['business_name'] : "";
-			}   */
-			/*(isset($_GET['status'])) ? $where['status'] = $_GET['status'] : "";
-			(isset($_GET['featured'])) ? $where['featured'] = $_GET['featured'] : "";
-			(isset($_GET['domain'])) ? $where['domain'] = $_GET['domain'] : "";
+			foreach($productCategory as $key => $value){
+				$productMenus[] = array("name" => $key, "url" => "/products/".str_replace(" ", "-",$key), "childMenu" => $value);
+			}
 			
-			$userCols['name'] = "name";
-			$userCols['username'] = "username";
-			$user = $db->getUsers($userId,$userCols);
-			$db->setLimit($limit);
-			$table = $db->setJoinString("INNER JOIN", "property", array("user_id"=>$user.".id"));
-			$db->setWhere($where, $table);
-			$db->setWhere($like, $table, true);
-			$selectInnerJoinCols[0] = "*";
-			$db->setColumns($table, $selectInnerJoinCols);
-			$data = $db->select();
+			foreach($serviceCategory as $key => $value){
+				$serviceMenus[] = array("name" => $key, "url" => "/services/".str_replace(" ", "-",$key), "childMenu" => $value);
+			}
 			
-			echo json_encode($data);
+			if(count($productMenus) >=1 || count($productMenus) >=1){
+				
+				$menus[] = array("name" => "Products", "url" => "/products", "childMenu" => $productMenus );
+				
+				$menus[] = array("name" => "Services", "url" => "/services", "childMenu" => $serviceMenus );
+			}
+			
 		}
-	}//end get
-	 */
-	
-	if($reqMethod=="POST"){
-		$insert = $db->insert("seo", $body);
-		echo json_encode($insert);
+		//print_r($menus);
+		echo json_encode($menus);
 	}
 	
-	if($reqMethod=="PUT" || $reqMethod=="DELETE"){
+	if($reqMethod == "PUT" || $reqMethod == "DELETE"){
 		$where['id'] = $id; // need where clause to update/delete record
-		$update = $db->update("property", $body, $where);
+		$update = $db->update("website", $body, $where);
 		echo json_encode($update);
 	}
  ?>
