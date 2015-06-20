5c3d920332197a3ea4b02f4c567122fd431991b9
Aarti <aarti@wtouch.in>
2015-06-20 16:49:21 +0530

    create new school1 folder

--- website/templates/educational_books/School1/js/script.js
+++ website/templates/educational_books/School1/js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 var hostUrl = '/website/templates/default/';
 
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp', ['ngRoute']);
 
-app.config(function($locationProvider) {
+app.config(function($routeProvider, $locationProvider) {
   /* $routeProvider
    .when('/:view', {
     templateUrl: function(rd) { return hostUrl+"/"+rd.view+'.html';}
   })
   .otherwise({ redirectTo: '/home' }); */
 });
-app.controller('enquiryController', function($scope,$http, $location) {
+app.controller('enquiryController', function($scope,$http, $route, $location) {
 	$scope.hostUrl = hostUrl;
 	var today = new Date();
 	var year = today.getFullYear();
@@ -30,12 +30,11 @@ app.controller('enquiryController', function($scope,$http, $location) {
 		});
 	};
 });	
-	app.controller('aboutController', function($scope,$http, $location) {
+	app.controller('aboutController', function($scope,$http, $route, $location) {
 		var s = $location.path();
 		$scope.url = s.substr(1);
 		$scope.makeActive = function(url){
 			$scope.id = url;
 		}
 		$scope.makeActive($scope.url);
-		
 	});
