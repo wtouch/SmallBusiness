0b8f8efdb2f7b0a27499677aba1a4c034e928c00
Vilas Shetkar <vilas@wtouch.in>
2015-06-21 09:58:27 +0530

    1) orange template in jewellery folder as DefaultJewellery
    2) default - Blue real estate template
    3) educational_books - school - empty, college - red real estate template.

--- website/templates/educational_books/college/js/script.js
+++ website/templates/educational_books/college/js/script.js
@@ -1,6 +1,41 @@
 'use strict';
 var hostUrl = '/website/templates/default/';
-
+$(document).ready(function(){
+	$('.bxslider').bxSlider({
+		mode:'horizontal',
+		slideWidth: 250,
+		minSlides:3,
+		maxSlides: 4,
+		slideMargin: 25,
+		auto: true, 
+		autoDirection:'next',
+		moveSlides: 2,
+		pause:2500,
+		pager:true,
+		pagerType:'full',
+		autoControls: true, 
+		controls:true, 
+		autoHover:true,
+		speed:1000,
+	});
+	$('.bxslidervertical').bxSlider({
+		mode:'vertical',
+		slideWidth: 600,
+		minSlides: 4,
+		maxSlides:4,
+		slideMargin: 15,
+		auto: true, 
+		autoDirection:'next',
+		moveSlides: 2,
+		pause:5000,
+		pager:false,
+		pagerType:'full',
+		autoControls: false, 
+		controls:false, 
+		autoHover:true,
+		speed:2000,
+	});
+});
 var app = angular.module('myApp',[]);
 
 app.config(function($locationProvider) {
