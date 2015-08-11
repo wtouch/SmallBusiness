bf0e01f9877bdb46a4d92e34782a5c2fb397cdba
Aarti <aarti@wtouch.in>
2015-08-11 11:28:02 +0530

    enquiry.html is modified.

--- website/templates/educational_books/schoolOrange/js/script1.js
+++ website/templates/educational_books/schoolOrange/js/script1.js
@@ -11,6 +11,7 @@ app.config(function($locationProvider) {
   .otherwise({ redirectTo: '/home' }); */
 });
 app.controller('enquiryController', function($scope,$http, $location) {
+	console.log('enquiryController');
 	$scope.hostUrl = hostUrl;
 	var today = new Date();
 	var year = today.getFullYear();
@@ -20,30 +21,14 @@ app.controller('enquiryController', function($scope,$http, $location) {
 	var min = today.getMinutes();
 	var sec = today.getSeconds();
 	$scope.mailSent = false;
-	
-	var params = {table : "config", config_name : "property"};
-	$http({
-		url: '/server-api/index.php/getmultiple/config/1/1',
-		method: "GET",
-		params: params
-	}).then(function (results) {
-		console.log(results);
-		if(results.data.status == 'success'){
-			$scope.propertyConfig = results.data.data.config_data;
-		}else{
-			$scope.propertyConfig = results.data.data;
-		}
-	});
 	$scope.enquiry = {
-		date : year + "-" + month + "-" + date + " " + hour + ":" + min + ":"+sec
-	};
+				subject : 'Website Enquiry',
+				date : year + "-" + month + "-" + date + " " + hour + ":" + min + ":"+sec
+			};
 	$scope.postData = function(enquiry){
-		$scope.loading = true;
-		console.log(enquiry);
-		  $http.post("/server-api/index.php/post/enquiry", $scope.enquiry).success(function(response) {
-			$scope.loading = false;
-			$scope.mailSent = true;
-		}); 
+		$http.post("/server-api/index.php/post/enquiry", $scope.enquiry).success(function(response) {
+				$scope.mailSent = true;
+		});
 	};
 });	
 	app.controller('aboutController', function($scope,$http, $location) {
