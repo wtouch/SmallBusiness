443c5d3d20c38149a5b6432a49d7ce76a4bfe2a3
Sunita <sunita@wtouch.in>
2015-09-02 11:53:34 +0530

    "homeproject modified in all templates,
    verified property display "

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "config.php";
 
-if($config['host'] == 'portal.local' || $config['host'] =="sunita.local"){
+if($config['host'] == 'portal.local' /*||  $config['host'] =="sunita.local" */){
 	require_once 'website/portal/index.php';
 }else{
 	require_once 'website/index.php';
