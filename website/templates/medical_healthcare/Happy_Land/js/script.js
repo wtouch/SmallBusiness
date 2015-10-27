388d5e8e13a211892d78c9af442a19ec800d3b4e
aditi <aditi@wtouch.in>
2015-10-27 12:00:33 +0530

    training.php creatrd

--- website/templates/medical_healthcare/Happy_Land/js/script.js
+++ website/templates/medical_healthcare/Happy_Land/js/script.js
@@ -160,3 +160,87 @@ app.controller('enquiryController', function($scope,$http, $location) {
 		$scope.makeActive($scope.url);
 		
 	});
+	
+/*********************************Traning controller***********************/	
+app.controller('trainingController', function($scope,$http, $location) {
+	$scope.hostUrl = hostUrl;
+	var today = new Date();
+	var year = today.getFullYear();
+	var month = today.getMonth() + 1;
+	var date = today.getDate();
+	var hour = today.getHours();
+	var min = today.getMinutes();
+	var sec = today.getSeconds();
+	
+	var params = {table : "training", config_name : "property"};
+	$http({
+		url: '/server-api/index.php/getmultiple/config/1/1',
+		method: "GET",
+		params: params
+	}).then(function (results) {
+		console.log(results);
+		if(results.data.status == 'success'){
+			$scope.propertyConfig = results.data.data.config_data;
+		}else{
+			$scope.propertyConfig = results.data.data;
+		}
+	});
+	$scope.mailSent = false;
+
+	$scope.enquiry = {
+		message : {
+			property_for :0
+		},
+		subject : 'Website Enquiry',
+		date : year + "-" + month + "-" + date + " " + hour + ":" + min + ":"+sec
+	};
+	$scope.postData = function(training){
+		$scope.loading = true;
+		console.log(training);
+		 $http.post("/server-api/index.php/post/training", $scope.training).success(function(response) {
+			 $scope.loading = false;
+			$scope.mailSent = true;
+		});
+	};
+	/*****************modalOptions*******************************/
+	$scope.openTrainingForm= function (url,traningData) {
+		var modalDefaults = {
+			templateUrl: url,	
+			size : 'lg'
+		};
+		var modalOptions = {
+			date : $scope.currentDate,
+			traningData : traningData,
+			
+		/*	getParty: function(modalOptions){
+				dataService.get("getmultiple/user/1/100", {status: 1, user_id : $rootScope.userDetails.id}).then(function(response){
+					modalOptions.customerList = (response.data);
+				});
+			},*/
+			postData : function(traningData) {
+			 dataService.post("post/traning",traningData)
+			.then(function(response) {  
+				if(response.status == "success"){
+					$scope.getContact($scope.CurrentPage);
+				}
+				 if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+				$notification[response.status]("Traning Details Added", response.message); 
+			});
+			}, 
+			updateData : function(id,traningData) {
+				dataService.put("put/traning/"+ contactData.id,traningData)
+				.then(function(response) {
+					if(response.status == "success"){
+						$scope.getContact($scope.CurrentPage);
+					}
+					if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+					$notification[response.status]("Record Updated", response.message);
+				});
+			} 
+		};
+		modalService.showModal(modalDefaults, modalOptions).then(function (result) {
+		});
+	};
+
+});	
+	
