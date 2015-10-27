9754fdbf7b8394efae47eee26c32e6786b8c5848
trupti <trupti@wtouch.in>
2015-10-27 05:58:52 +0530

    app.js
    singleview.html
    training.html
    trainingController.js

--- app/modules/training/trainingController.js
+++ app/modules/training/trainingController.js
@@ -11,6 +11,78 @@ var injectParams = ['$scope', '$injector','$routeParams','$rootScope','dataServi
 		$scope.numPages = "";		
 		$scope.user_id = $rootScope.userDetails.id;
 		console.log($rootScope.userDetails.id);
+		
+		$scope.openForm = function (url,addtraining) {
+			var x = angular.copy(addtraining);
+			var modalDefaults = {
+				templateUrl: url,	// apply template to modal
+				size : 'lg'
+			};
+			$scope.isSend = false;
+			var modalOptions = {
+				addtraining : (addtraining) ? x : {},
+				postData : function(addtraining){
+					dataService.post("post/training",addtraining).then(function(response) {
+						if(response.status=='success'){
+							$scope.getTraining(currentPage, user_id,trainingParam);
+					}
+					if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+						$notification[response.status]("Add Record", response.message);
+					}); 
+				},
+				
+				updateData : function(addtraining) {
+					$scope.addtraining =(addtraining);
+					delete addtraining.admin;
+					delete addtraining.manager;
+					delete addtraining.userCount;
+					delete addtraining.username;
+					delete addtraining.admin_id;
+					//delete addtraining.admin_id;
+					console.log($scope.addtraining);
+					dataService.put("put/training/"+addtraining.id,addtraining)
+					.then(function(response) {
+						if(response.status == "success"){
+							
+						}
+						if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+						$notification[response.status]("Submit Custom Template", response.message);
+					}); 
+				}
+			};
+			//console.log(response.data);
+			modalService.showModal(modalDefaults, modalOptions).then(function (result) {
+				console.log("modalOpened");
+			});
+		};	
+		
+		$scope.searchFilter = function(statusCol, searchProp,user_id) {
+			$scope.filterStatus= {search: true};
+			(searchProp =="") ? delete $scope.trainingParam[statusCol] : $scope.filterStatus[statusCol] = searchProp;
+			angular.extend($scope.trainingParam, $scope.filterStatus);
+			$scope.getTraining(1,user_id,$scope.trainingParam);
+		};
+	/***************************************************************************************/
+	// code for filter data as per satus (delete/active)		
+		$scope.changeStatus = function(statusCol, showStatus,user_id) {
+			$scope.filterStatus= {};
+			(showStatus =="") ? delete $scope.trainingParam[statusCol] : $scope.filterStatus[statusCol] = showStatus;
+			angular.extend($scope.trainingParam, $scope.filterStatus);
+			dataService.get("getmultiple/training/1/"+$scope.pageItems, $scope.trainingParam)
+			.then(function(response) {  
+				if(response.status == 'success'){
+					$scope.training = response.data;
+					$scope.totalRecords = response.totalRecords;
+				}else{
+					$scope.training = {};
+					$scope.totalRecords = {};
+					if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+					$notification[response.status](response.message);
+				}
+			});
+		};
+	/****************************************************************************************/
+		
 		$scope.getTraining = function(page, user_id, trainingParam){
 			$scope.trainingParam = (trainingParam) ? trainingParam : {
 					status : 1,
@@ -19,7 +91,8 @@ var injectParams = ['$scope', '$injector','$routeParams','$rootScope','dataServi
 			
 			dataService.get("/getmultiple/training/"+page+"/"+$scope.pageItems, $scope.trainingParam)
 			.then(function(response) {
-				if(response.status == 'success'){				
+				if(response.status == 'success'){
+					//$scope.viewData.technologies = Json_stringify(modalOptions.viewData.technologies);
 					$scope.totalRecords = response.totalRecords;
 					$scope.training = response.data;
 					console.log(response);
