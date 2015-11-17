adfb2f7fbc166284125d5530981604bf4c632530
sunita <sunita@wtouch.in>
2015-11-17 19:16:18 +0530

    global get,post,put method created

--- server-api/index1.php
+++ server-api/index1.php
@@ -16,73 +16,12 @@ $app->response->headers->set('Content-Type', 'application/json');
 // this will get input
 $body = $app->request->getBody();
 
-/* $app->get('/notfound', function()use ($app, $baseUrl){
-	echo "Not Found";
-}); */
-//use these uri for all get requests {Vilas}
-$app->post('/upload(/:postParams)', 'uploadFiles' );
-$app->post('/excel(/:postParams)', 'excelFiles' );
-$app->post('/sendmail(/:postParams)', 'sendMail' );
-
-//Use this uri for multiple records with limit {Vilas}
+//global request 	{sunita}
 $app->get('/','getRecords');
-
-$app->get('/login/:getRequest','login');
-
-// use thi uri for single record {Vilas}
-$app->get('/getsingle/:getRequest(/:id)', 'getRecord' );
-//use this uri for post new record into database - like create
 $app->post('/', 'postRecord' );
-//use this uri for put/update record from database
-$app->put('/put/:getRequest/:id', 'putRecord' );
-//use this uri for delete record from database
-$app->delete('/delete/:getRequest/:id', 'deleteRecord' );
-
-function login($getRequest){
-	$app = new \Slim\Slim();
-	$body = $app->request->getBody();
-	// this will get current url
-	/* $posIndex = strpos( $_SERVER['PHP_SELF'], '/index.php');
-	$baseUrl = substr( $_SERVER['PHP_SELF'], 0, $posIndex).'/index.php'; */ 
+$app->put('/', 'putRecord' );
 	
-	try{
-			include 'modules/user.php';
-	}
-	catch(Exception $e) {
-       $response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
-		echoResponse(200, $response);
-    }
-};
-
-function getRecord($getRequest, $id=null){
-	$app = new \Slim\Slim();
-	$body = $app->request->getBody();
-	// this will get current url
-	/* $posIndex = strpos( $_SERVER['PHP_SELF'], '/index.php');
-	$baseUrl = substr( $_SERVER['PHP_SELF'], 0, $posIndex).'/index.php'; */ 
-	
-	$id = (int)$id;
-	
-	try{
-		if($id === 0){
-			if($id === 0){
-				throw new Exception('Please Use proper id for record.');
-			}
-		}else{
-			include 'modules/'.$getRequest.'.php';
-		}
-	}
-	catch(Exception $e) {
-        $response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
-		echoResponse(200, $response);
-    }
-};
-
-//this function will help to add modules & fetch records from database {vilas}
+//global  request for get record {sunita}
 function getRecords(){
 	$app = new \Slim\Slim();
 	$body = $app->request->getBody();
@@ -92,179 +31,76 @@ function getRecords(){
 		$table = $db->setTable($_GET['table']);
 		$single = $_GET['single'];
 		$cols = $params['cols'];
-		//$search = $params['search'];
-		 // For single request
-		
-			if($params['limit']){
-				$limit[0] = $params['limit']['page'];
-				$limit[1] = $params['limit']['records'];
-			}
-			if(isset($params['search']) && ($params['search'] == true)){
-				(isset($_GET['name'])) ? $like['name'] = $_GET['name'] : "";
-			} 
-			$where = array();
+		// For limited records
+		if(isset($params['limit'])){
+			$limit[0] = $params['limit']['page'];
+			$limit[1] = $params['limit']['records'];
 			$db->setLimit($limit);
-		
-			if(isset($params['where'])){
-				$db->setWhere($params['where'], $table,true);
-			}
-		
-			$db->setColumns($table,$cols);
-			//print_r($db->setColumns($table,$cols));
-			$data = $db->select();
-			//print_r($data);
-			echo json_encode($data);
-		//}else{ // For Multiple request
-			
-		//}
-	}
-	catch(Exception $e) {
-        //return $app->response()->redirect($baseUrl.'/notfound');
+		}
+		//for filter
+		if(isset($params['search'])){
+			$db->setWhere($params['search'], $table, true);
+		}
+
+		if(isset($params['where'])){
+			$db->setWhere($params['where'], $table,true);
+		}
+		$db->setColumns($table,$cols);
+		$data = $db->select();
+		echo json_encode($data);
+	}catch(Exception $e) {
 		$response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
+		$response["message"] = "Error: '".$e->getMessage()."'";
+		$response["data"] = null;
 		echoResponse(200, $response);
-    }
-		
+	}
 };
 
+// global request for post record
 function postRecord(){
 	$app = new \Slim\Slim();
 	$body = $app->request->getBody();
-	// this will get current url
-	$posIndex = strpos( $_SERVER['PHP_SELF'], '/index1.php');
-	$baseUrl = substr( $_SERVER['PHP_SELF'], 0, $posIndex).'/index1.php'; 
-	
+	$db = new dbHelper();
+	$table = $_GET['table'];
 	try{
-		
-		/* if(!isset($_SESSION['username']) && $postParams != ("login" || "forgotpass" || "changepass") && $getRequest != "enquiry" && $getRequest != "training"){
-			throw new Exception('You are not logged in!');
-		} */
 		if($body===""){
 			throw new Exception('There is no input!');
 		}else{
-			include 'modules/user.php';
+			$insert = $db->insert($table, $body);
+			echo json_encode($insert);
 		}
 	}
 	catch(Exception $e) {
-		//echo $postParams;
 		$response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-		//print_r( $response["message"]);
-        $response["data"] = null;
+		$response["message"] = "Error: '".$e->getMessage()."'";
+		$response["data"] = null;
 		echoResponse(200, $response);
-    }
+	}
 };
-
-function uploadFiles($postParams = null){
-	$app = new \Slim\Slim();
-	$body = $app->request->getBody();
-
-	try{
-		if(!isset($_FILES)){
-				throw new Exception('There is no file input!');
-		}else{
-			include 'modules/upload.php';
-		}
-	}
-	catch(Exception $e) {
-		$response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
-		echoResponse(200, $response);
-    }
-}
-function excelFiles($postParams = null){
-	$app = new \Slim\Slim();
-	$body = $app->request->getBody();
-	//echo $body;
-	try{
-		if(!isset($_FILES)){
-				throw new Exception('There is no file input!');
-		}else{
-			include 'modules/excel.php';
-		}
-	}
-	catch(Exception $e) {
-		$response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
-		echoResponse(200, $response);
-    }
-}
-function sendMail($postParams = null){
-	$app = new \Slim\Slim();
-	$body = $app->request->getBody();
-
-	try{
-			include 'modules/mail.php';
-	}
-	catch(Exception $e) {
-		$response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
-		echoResponse(200, $response);
-    }
-}
-function putRecord($getRequest, $id){
-	$app = new \Slim\Slim();
-	$body = $app->request->getBody();
-	//print_r($body);
-	// this will get current url
-	$posIndex = strpos( $_SERVER['PHP_SELF'], '/index.php');
-	$baseUrl = substr( $_SERVER['PHP_SELF'], 0, $posIndex).'/index.php'; 
 	
-	$id = (int)$id;
-	try{
-		if(!isset($_SESSION['username'])){
-			throw new Exception('You are not logged in!');
-		}
-		if($id === 0 || $getRequest===null){
-			if($id === 0){
-				throw new Exception('Please Use proper id for record.');
-			}
-			if($getRequest===null){
-				throw new Exception('Please Use proper Module Name for record.');
-			}
-		}else{
-			include 'modules/'.$getRequest.'.php';
-		}
-	}
-	catch(Exception $e) {
-        $response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
-		echoResponse(200, $response);
-    }
-};
-
-function deleteRecord($getRequest, $id){
+//global method for update or delete record
+function putRecord(){
 	$app = new \Slim\Slim();
 	$body = $app->request->getBody();
-	// this will get current url
-	$posIndex = strpos( $_SERVER['PHP_SELF'], '/index.php');
-	$baseUrl = substr( $_SERVER['PHP_SELF'], 0, $posIndex).'/index.php'; 
-	
-	$id = (int)$id;
+	$db = new dbHelper();
+	$params = json_decode($_GET['params'],true);
+	$table = $_GET['table'];
 	try{
-		if($id === 0 || $getRequest===null){
-			if($id === 0){
-				throw new Exception('Please Use proper id for record.');
-			}
-			if($getRequest===null){
-				throw new Exception('Please Use proper Module Name for record.');
-			}
+		if(!isset($params['where'])){
+			throw new Exception('Please Use proper id for record.');
 		}else{
-			include 'modules/'.$getRequest.'.php';
+			$update = $db->update($table, $body, $params['where']);
+			echoResponse(200, $update);
 		}
 	}
 	catch(Exception $e) {
-       $response["status"] = "error";
-        $response["message"] = "Error: '".$e->getMessage()."'";
-        $response["data"] = null;
+		$response["status"] = "error";
+		$response["message"] = "Error: '".$e->getMessage()."'";
+		$response["data"] = null;
 		echoResponse(200, $response);
-    }
-};
+	}
+}
+
 function echoResponse($status_code, $response) {
     $app = \Slim\Slim::getInstance();
     // Http response code
