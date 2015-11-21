cb07108d7d309050ac52d4291f7b0b4a6cecb185
manisha karande <manisha@wtouch.in>
2015-11-21 13:27:42 +0530

    added some ignored files.

---
+++ website/templates/default/default/js/script.js
@@ -0,0 +1,99 @@
+'use strict';
+var hostUrl = '/website/templates/default/';
+jQuery(document).ready(function() {
+	jQuery(".subimgs").click(function(){
+		jQuery(".mainimgs").attr("src",(jQuery(this).attr("src")))
+	})
+});
+var app = angular.module('myApp',[]);
+$(document).ready(function(){
+	$('#bxslider1').bxSlider({
+		mode:'vertical',
+		minSlides: 1,auto: true, 
+		autoDirection:'next',
+		moveSlides: 1,
+		pause:4000,
+		pager:false,
+		pagerType:'full',
+		autoControls: false, 
+		controls:false, 
+		autoHover:true,
+		speed : 1000
+	});
+	$('#bxslider').bxSlider({
+		mode:'horizontal',
+		slideMargin: 5,
+		minSlides :1,
+		maxSlides: 4,
+		moveSlides: 2,
+		slideWidth: 200,
+		auto: true, 
+		autoDirection:'next',
+		pause:4000,
+		pager:false,
+		pagerType:'full',
+		autoControls: false, 
+		controls:true, 
+		autoHover:true
+	});
+	$('#bxslider3').bxSlider({
+		mode:'horizontal',
+		slideMargin: 5,
+		minSlides :1,
+		maxSlides: 4,
+		moveSlides: 2,
+		slideWidth: 200,
+		auto: true, 
+		autoDirection:'next',
+		pause:4000,
+		pager:false,
+		pagerType:'full',
+		autoControls: false, 
+		controls:true, 
+		autoHover:true
+	});
+});
+app.config(function($locationProvider) {
+  /* $routeProvider
+   .when('/:view', {
+    templateUrl: function(rd) { return hostUrl+"/"+rd.view+'.html';}
+  })
+  .otherwise({ redirectTo: '/home' }); */
+});
+app.controller('enquiryController', function($scope,$http, $location) {
+	$scope.hostUrl = hostUrl;
+	var today = new Date();
+	var year = today.getFullYear();
+	var month = today.getMonth() + 1;
+	var date = today.getDate();
+	var hour = today.getHours();
+	var min = today.getMinutes();
+	var sec = today.getSeconds();
+	$scope.mailSent = false;
+	//$scope.loading = true;
+	$scope.enquiry = {
+				subject : 'Website Enquiry',
+				date : year + "-" + month + "-" + date + " " + hour + ":" + min + ":"+sec
+			};
+	$scope.postData = function(enquiry){
+		$http.post("/server-api/index.php/post/enquiry", $scope.enquiry).success(function(response) {
+			$scope.mailSent = true;
+				/* if(response=='success'){
+					$scope.loading = false;
+					$scope.mailSent = true;
+				}
+				else{
+					alert(response);
+				} */
+		});
+	};
+});	
+	app.controller('aboutController', function($scope,$http, $location) {
+		var s = $location.path();
+		$scope.url = s.substr(1);
+		$scope.makeActive = function(url){
+			$scope.id = url;
+		}
+		$scope.makeActive($scope.url);
+		
+	});
