ca4a72417e0d30337d56c2faee5769184700cf32
Vidya <vidya@wtouch.in>
2015-11-23 18:03:21 +0530

    navbar and home project colors changed..

---
+++ website/templates/educational_books/realtwentyseven/js/script.js
@@ -0,0 +1,147 @@
+'use strict';
+var hostUrl = '/website/templates/default/';
+jQuery(document).ready(function() {
+	jQuery(".subimgs").click(function(){
+		jQuery(".mainimgs").attr("src",(jQuery(this).attr("src")))
+	})
+	$(".block-menu >li >a").addClass('three-d').append(function(n,txt){
+
+            return '<span class="three-d-box"><span class="front">'+ txt +'</span><span class="back">'+ txt +'</span></span>';
+
+        });
+});
+
+			jQuery(document).ready(function(){
+
+		jQuery('#featuredProject,#featuredProp,#featuredService, #featuredProduct').carousel({
+
+		  interval: 4000
+
+		})
+
+		jQuery('.carousel .item').each(function(){
+
+		  var next = jQuery(this).next();
+
+		  if (!next.length) {
+
+			next = jQuery(this).siblings(':first');
+
+		  }
+
+		  next.children(':first-child').clone().appendTo(jQuery(this));
+
+		  for (var i=0;i<2;i++) {
+
+			next=next.next();
+
+			if (!next.length) {
+
+				next = jQuery(this).siblings(':first');
+
+			}
+
+			next.children(':first-child').clone().appendTo(jQuery(this));
+
+		  }
+
+		});	
+
+	});
+$(document).ready(function(){
+	$('.carousslider').bxSlider({
+		mode:'fade',
+		minSlides: 1,
+		auto: true, 
+		autoDirection:'next',
+		moveSlides: 1,
+		pause:4000,
+		pager:false,
+		pagerType:'full',
+		autoControls: false, 
+		controls:false, 
+		autoHover:true,
+		speed : 1000
+	});
+	$('.bxslider').bxSlider({
+		mode:'horizontal',
+		slideMargin: 10,
+		minSlides :1,
+		maxSlides: 3,
+		moveSlides: 2,
+		slideWidth: 250,
+		auto: true, 
+		autoDirection:'next',
+		pause:4000,
+		pager:false,
+		pagerType:'full',
+		autoControls: false, 
+		controls:true, 
+		autoHover:true
+	});
+});
+var app = angular.module('myApp',[]);
+
+app.config(function($locationProvider) {
+  /* $routeProvider
+   .when('/:view', {
+    templateUrl: function(rd) { return hostUrl+"/"+rd.view+'.html';}
+  })
+  .otherwise({ redirectTo: '/home' }); */
+});
+app.controller('enquiryController', function($scope,$http, $location) {
+	$scope.hostUrl = hostUrl;
+	var today = new Date();
+	var year = today.getFullYear();
+	var month = today.getMonth() + 1;
+	var date = today.getDate();
+	var hour = today.getHours();
+	var min = today.getMinutes();
+	var sec = today.getSeconds();
+	
+	var params = {table : "config", config_name : "property"};
+	$http({
+		url: '/server-api/index.php/getmultiple/config/1/1',
+		method: "GET",
+		params: params
+	}).then(function (results) {
+		if(results.data.status == 'success'){
+			$scope.propertyConfig = results.data.data.config_data;
+		}else{
+			$scope.propertyConfig = results.data.data;
+		}
+	});
+	
+	// Don't change following code
+	/* start email code */
+	$scope.mailSent = false;
+	$scope.enquiry = {
+		date : year + "-" + month + "-" + date + " " + hour + ":" + min + ":"+sec
+	};
+	$scope.postData = function(enquiry){
+		$scope.loading = true;
+		if(enquiry.message.property_link){
+			enquiry.message.property_link = "<a href=\""+ location.origin +"/properties/"+ enquiry.message.property_link.params.property_title + "/" + enquiry.message.property_link.params.property_id + "\">"+enquiry.message.property_link.params.property_title+"</a>";
+		}
+		$http.post("/server-api/index.php/post/enquiry", $scope.enquiry).success(function(response) {
+			$scope.loading = false;
+			if(response.status == "success"){
+				$scope.mailSent = true;
+			}else{
+				$scope.mailSent = false;
+				$scope.errorMessage = response.message;
+				console.log(response);
+			}
+		});
+	};
+	/* End email code */
+});	
+	app.controller('aboutController', function($scope,$http, $location) {
+		var s = $location.path();
+		$scope.url = s.substr(1);
+		$scope.makeActive = function(url){
+			$scope.id = url;
+		}
+		$scope.makeActive($scope.url);
+		
+	});
