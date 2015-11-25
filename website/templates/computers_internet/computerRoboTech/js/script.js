8fa192824a4f3494491ab207f1941ae3ecb47499
pallavi <pallavi@wtouch.in>
2015-11-25 16:42:10 +0530

    navbar changed

--- website/templates/computers_internet/computerRoboTech/js/script.js
+++ website/templates/computers_internet/computerRoboTech/js/script.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 	jQuery(".subimgs").click(function(){
 		jQuery(".mainimgs").attr("src",(jQuery(this).attr("src")))
 	})
-	/* $(window).scroll(function(e){
+	 $(window).scroll(function(e){
 		var scrollTop = $(document).scrollTop();
 		if(scrollTop > 0){
 			console.log(scrollTop);
 			$('nav').removeClass('navbar-static-top').addClass("navbar-fixed-top");
 		} else {
-			$('nav').removeClass("navbar-fixed-top").addClass('navbar-static-top');
+			$('nav').removeClass("navbar-fixed-top").addClass('navbar-static-top')
 		}
-	}); */
+	}); 
 	
 	// This is for Cookie
 	function getCookie(cname) {
