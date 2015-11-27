9f85505fd88fff6325bc8a6d2a6b38e8d10905fa
Amit Bajaj <amit@wtouch.in>
2015-11-27 18:18:49 +0530

    about us robotech changed

--- website/templates/furniture_products/interiortwo/js/script.js
+++ website/templates/furniture_products/interiortwo/js/script.js
@@ -12,7 +12,9 @@ $(document).ready(function(){
 	$(".content2").height($(window).height() - $(".content1").outerHeight() - $("footer").outerHeight());
 	console.log($(window).height() - $(".content1").outerHeight() - $("footer").outerHeight());
 	
-	
+	$(window).resize(function(){
+		$(".content2").height($(window).height() - $(".content1").outerHeight() - $("footer").outerHeight());
+	});
 	
 	var sliderMode, minSlides, maxSlides;
 	//$(window).resize(function(){
