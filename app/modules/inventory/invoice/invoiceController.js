c5021d3b1f5c1c42c0bf4972491a690566fa2675
aditi <aditi@wtouch.in>
2015-11-27 15:59:19 +0530

    payInvoice.html added

--- app/modules/inventory/invoice/invoiceController.js
+++ app/modules/inventory/invoice/invoiceController.js
@@ -21,16 +21,16 @@ define(['app'], function (app) {
 			enableSorting: true,
 			enableFiltering: true,
 			columnDefs: [
-				{ name:'SrNo', 
+				{ name:'SrNo', width:50,
 					cellTemplate : "<span>{{ (grid.appScope.pageItems * (grid.appScope.currentPage - 1)) + rowRenderIndex + 1}}</span>",enableSorting: false,enableFiltering: false
 					
 				},
-				{ name:'id',enableSorting: false ,
+				{ name:'id',  width:50, enableSorting: false ,
 				filterHeaderTemplate: '<input id="id" class="form-control" ng-change="grid.appScope.filter(\'id\', id, \'invoice\', \'invoiceList\')" ng-model="id" placeholder="Id">',
 				},
 				
 				{
-					name:'User Name',
+					name:'User Name', width:150,
 					filterHeaderTemplate: '<select class="form-control" ng-change="grid.appScope.filter(\'user_id\', partyFilter, \'party\', \'partyList\')" ng-model="partyFilter" ng-options="item.id as item.name for item in grid.appScope.partyList">'
 							+'<option value="" selected>Party Name</option>'
 						+'</select>',
@@ -39,7 +39,7 @@ define(['app'], function (app) {
 					}
 				},
 				{
-					name:'Account Name',
+					name:'Account Name', width:150,
 					filterHeaderTemplate: '<select id="account_name" class="form-control" ng-change="grid.appScope.filter(\'account_no\', account_name, \'account\', \'accountList\')" ng-model="account_name" ng-options="item.id as item.account_name for item in grid.appScope.accountList">'
 							+'<option value="" selected>Account Name</option>'
 						+'</select>',
@@ -47,13 +47,27 @@ define(['app'], function (app) {
 					  placeholder: 'Account Name'
 					}
 				},
-				{ name:'generated_date', enableSorting: false,
+				{ name:'generated_date',width:150, enableSorting: false,
 				filterHeaderTemplate: '<input id="generated_date" class="form-control" ng-change="grid.appScope.filter(\'generated_date\', generated_date, \'invoice\', \'invoiceList\')" ng-model="generated_date" placeholder="Date">',
 				},
-				{ name:'due_date', enableSorting: false,
+				{ name:'due_date', width:150, enableSorting: false,
 				filterHeaderTemplate: '<input id="due_date" class="form-control" ng-change="grid.appScope.filter(\'due_date\', due_date, \'invoice\', \'invoiceList\')" ng-model="due_date" placeholder="Date">',
 					},
-				{ name:'status',
+				{ 
+					name:'amount',width:80,
+					enableSorting: false ,
+					enableFiltering: false, 
+					cellTemplate : "<span>{{row.entity.particulars[0].amount}}</span>"
+				},
+				{
+					name:'price',width:80,
+					enableSorting: false ,
+					enableFiltering: false, 
+					cellTemplate : "<span>{{row.entity.particulars[0].price}}</span>"
+					},
+				{
+					name:'Manage', 
+					enableSorting: false, 
 					filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'invoice\', \'invoiceList\')" ng-model="status">'
 							+'<option value="" selected>Status</option>'
 							+'<option value="0">Deleted</option>'
@@ -63,16 +77,10 @@ define(['app'], function (app) {
 					  //type: uiGridConstants.filter.SELECT,
 					 
 					  options: [ { value: '1', label: 'Active' }, { value: '0', label: 'Delete' }]
-					} 
-				},
-				{ 
-				name:'amount',
-				enableSorting: false , 
-				cellTemplate : "<span>{{row.entity.particulars[0].amount}}</span>"},
-				{ name:'price',enableSorting: false , enableFiltering: false, cellTemplate : "<span>{{row.entity.particulars[0].price}}</span>"},
-				{ name:'Manage', enableSorting: false, enableFiltering: false, 
+					} ,
 					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/inventory/invoice/addinvoice.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="Edit Account Information"> <span class="glyphicon glyphicon-pencil"></span></a>'
 					+ '<a type="button" tooltip="Delete Account" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'invoice\', \'status\',row.entity.status, row.entity.id)" btn-checkbox="" btn-checkbox-true="1" btn-checkbox-false="0" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'
+					+'<a ng-click="grid.appScope.openModal(\'modules/inventory/invoice/payInvoice.html\')" class="btn btn-info btn-sm" type="button" tooltip-animation="true" tooltip="Pay Invoice Information"> <span class="glyphicon glyphicon-usd"></span></a>'
 					
 				}
 			],
@@ -93,14 +101,19 @@ define(['app'], function (app) {
 			var modalOptions = {
 					date : $scope.currentDate,
 					addinvoice : (data) ? {
-					id : data.id,
-					generated_date : data.generated_date,
-					due_date : data.due_date,
-					particulars:data.particulars,
-					remark : data.remark
-				} : {
-					//generated_date : dataService.sqlDateFormate()
-				},
+						id : data.id,
+						generated_date : data.generated_date,
+						//due_date : data.due_date,
+						particulars:data.particulars,
+						remark : data.remark
+					} : {
+						//generated_date : dataService.sqlDateFormate()
+					},
+					invoicePayment: (data) ? {
+						pay_date : data.pay_date,
+					} : {
+						//generated_date : dataService.sqlDateFormate()
+					},
 				
 				postData : function(table, input){
 					$rootScope.postData(table, input,function(response){
@@ -122,6 +135,19 @@ define(['app'], function (app) {
 						}
 					})
 				},
+				subTotal : 0,
+				totalCalculate : function(modalOptions){
+					modalOptions.subTotal = 0;
+					//modalOptions.total_amount = 0;
+					//modalOptions.tax = {service_tax:0,other_tax:0,tds:0};
+					for(var x in modalOptions.addInvoice.singleparticular){
+						//modalOptions.tax = dataService.calculateTax(modalOptions.singleparticular.particulars[x].tax, modalOptions.singleparticular.singleparticular[x].amount,modalOptions.tax);
+						modalOptions.subTotal += modalOptions.singleparticular.particulars[x].amount;
+						//modalOptions.singleparticular.total_amount =   modalOptions.singleparticular.price + modalOptions.singleparticular.quantity;
+					}
+					return modalOptions;
+				},
+			
 				updateData : function(table, input, id){
 					$rootScope.updateData(table, input, id, function(response){
 						if(response.status == "success"){
