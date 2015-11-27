27a8b3337efea23b6b9785550e5952bb7b067a52
Amit Bajaj <amit@wtouch.in>
2015-11-27 13:03:17 +0530

    footer changed

--- website/templates/furniture_products/interiortwo/js/script.js
+++ website/templates/furniture_products/interiortwo/js/script.js
@@ -12,9 +12,7 @@ $(document).ready(function(){
 	$(".content2").height($(window).height() - $(".content1").outerHeight() - $("footer").outerHeight());
 	console.log($(window).height() - $(".content1").outerHeight() - $("footer").outerHeight());
 	
-	$(window).resize(function(){
-		$(".content2").height($(window).height() - $(".content1").outerHeight() - $("footer").outerHeight());
-	});
+	
 	
 	var sliderMode, minSlides, maxSlides;
 	//$(window).resize(function(){
