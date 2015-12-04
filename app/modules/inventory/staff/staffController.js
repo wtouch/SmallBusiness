7652b8079ebd5aba8554af06cafb0721939aaa29
manisha karande <manisha@wtouch.in>
2015-12-04 18:03:55 +0530

    create addstaff module

--- app/modules/inventory/staff/staffController.js
+++ app/modules/inventory/staff/staffController.js
@@ -28,14 +28,70 @@ define(['app'], function (app) {
 					}
 				}
 			},
-			
+			{
+				name : "Add Attendance",
+				path : "#/dashboard/inventory/staff/",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/inventory/staff/addattendance.html");
+					}
+				}
+			},
+			{
+				name : "Add Leaves",
+				path : "#/dashboard/inventory/staff/",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/inventory/staff/addleaves.html");
+					}
+				}
+			},
+			{
+				name : "Pay by Staff",
+				path : "#/dashboard/inventory/staff/",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/inventory/staff/pay_by_staff.html");
+					}
+				}
+			},
+			{
+				name : "Pay for Staff",
+				path : "#/dashboard/inventory/staff/",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/inventory/staff/pay_for_staff.html");
+					}
+				}
+			},
+			{
+				name : "Holidays",
+				path : "#/dashboard/inventory/staff/",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/inventory/staff/holidays.html");
+					}
+				}
+			},
+			{
+				name : "View Holidays",
+				path : "#/dashboard/inventory/staff/",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/inventory/staff/view_holiday.html");
+					}
+				}
+			},
 		]
 		
 		$scope.staff = {
 			enableSorting: true,
 			enableFiltering: true,
 			columnDefs: [
-				{ name:'SrNo',width:50,enableSorting: false,enableFiltering: false, 
+				{ name:'SrNo',
+				  width:50,
+				  enableSorting: false,
+				  enableFiltering: false, 
 					cellTemplate : "<span>{{ (grid.appScope.pageItems * (grid.appScope.currentPage - 1)) + rowRenderIndex + 1}}</span>",
 					filter: {
 					  //placeholder: 'ends with'
@@ -43,35 +99,43 @@ define(['app'], function (app) {
 				},
 				
 					{
-				    name:'name',width:200,
+				    name:'name',
+					width:200,
 					filterHeaderTemplate: '<input id="name" class="form-control" ng-change="grid.appScope.filter(\'name\', name, \'staff\', \'staff\',true)" ng-model="name" placeholder="Name">',
                 },
 				{
-				    name:'email',width:130,
+				    name:'email',
+					width:130,
 					filterHeaderTemplate: '<input id="email" class="form-control" ng-change="grid.appScope.filter(\'email\', email, \'staff\', \'staff\',true)" ng-model="email" placeholder="Email">'
                 },
 				
 			   {
-				    name:'address',width:130,
+				    name:'address',
+					width:130,
 					filterHeaderTemplate: '<input id="address" class="form-control" ng-change="grid.appScope.filter(\'address\', address, \'staff\', \'staff\',true)" ng-model="address" placeholder="Address">',
                 },
 				
 				
 				{
-				    name:'city',width:90,
-				     filterHeaderTemplate: '<input id="city" class="form-control" ng-change="grid.appScope.filter(\'city\', city, \'staff\', \'staff\',true)" ng-model="city" placeholder="City">', 
+				 name:'city',
+				 width:90,
+				 filterHeaderTemplate: '<input id="city" class="form-control" ng-change="grid.appScope.filter(\'city\', city, \'staff\', \'staff\',true)" ng-model="city" placeholder="City">', 
                 },
-				{ name:'designation',width:100,
+				{ 
+				  name:'designation',
+				  width:100,
 					filterHeaderTemplate: '<input id="designation" class="form-control" ng-change="grid.appScope.filter(\'designation\', designation, \'staff\', \'staff\',true)" ng-model="designation" placeholder="Designation">'
 				},
-				{ name:'department',width:120,
-					filterHeaderTemplate: '<select id="department" class="form-control" ng-change="grid.appScope.filter(\'department\', department, \'staff\', \'staff\',true)" ng-model="department">'
+				{ 
+				  name:'department',
+				  width:120,
+				  filterHeaderTemplate: '<select id="department" class="form-control" ng-change="grid.appScope.filter(\'department\', department, \'staff\', \'staff\',true)" ng-model="department">'
 							+'<option value="" selected>--Select--</option>' 
 							+'<option value="IT">IT</option>'
 							+'<option value="CIVIL">CIVIL</option>'
 							+'<option value="MECH">MECH</option>'
 						+'</select>', 
-				 filter: {
+				  filter: {
 					   type: uiGridConstants.filter.SELECT,  
 					  selectOptions: [ { value: 'IT', label: 'IT' }, { value: 'CIVIL', label: 'CIVIL'},{ value: 'MECH', label: 'MECH' }]
 					} 
@@ -79,8 +143,10 @@ define(['app'], function (app) {
 				},
 				
 				
-				 { name:'Manage', width:250,
-				filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'staff\', \'staff\')" ng-model="status">'
+				 { 
+				 name:'Manage',
+				 width:250,
+				 filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'staff\', \'staff\')" ng-model="status">'
 							 +'<option value="" selected>--Select--</option>' 
 							+'<option value="0">Deleted</option>'
 							+'<option value="1">Active</option>	'
@@ -89,24 +155,20 @@ define(['app'], function (app) {
 					   type: uiGridConstants.filter.SELECT,  
 					  selectOptions: [ { value: '1', label: 'Active' }, { value: '0', label: 'Deleted' }
 					  ]
-					} 
-				,
-				 
-					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/addstaff.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="Edit staff" > <span class="glyphicon glyphicon-pencil"></span></a>'
-					
-					+ '<a type="button" tooltip="Delete record" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'staff\', \'status\',row.entity.status, row.entity.id)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'
-					
-					+'<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/viewpayslip.html\')" class="btn btn-primary btn-sm btn btn-warning" type="button" tooltip-animation="true" tooltip="view payslip" > <span class="glyphicon glyphicon-eye-open"></span></a>'
+					} ,
+					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/viewpayslip.html\')" class="btn btn-primary btn-sm btn btn-warning" type="button" tooltip-animation="true" tooltip="view payslip" > <span class="glyphicon glyphicon-eye-open"></span></a>'
 					
 					+'<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/viewleaves.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="view leaves"><span class="glyphicon glyphicon-eye-open"></span></a>'
 					
 					+'<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/view_staff.html\',row.entity)" class="btn btn-primary btn-sm btn btn-warning" type="button" tooltip-animation="true" tooltip="View Staff"><span class="glyphicon glyphicon-eye-open"></span></a>'
 					
 					+'<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/staff_attendence.html\',row.entity)" class="btn btn-primary btn-sm btn" type="button" tooltip-animation="true" tooltip="Attendence"><span class="glyphicon glyphicon-user"></span></a>'
+					+'<a ng-click="grid.appScope.openModal(\'modules/inventory/staff/addstaff.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="Edit staff" > <span class="glyphicon glyphicon-pencil"></span></a>'
+					
+					+ '<a type="button" tooltip="Delete record" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'staff\', \'status\',row.entity.status, row.entity.id)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'
 				} 
 			]
 		};
-		
 		$scope.openModal = function(url,data){
 			var modalDefault = {
 				templateUrl: url,	// apply template to modal
@@ -138,8 +200,8 @@ define(['app'], function (app) {
 					staff_id:data.staff_id,
 					modified_date:data.modified_date,
 					registration_date:data.registration_date
-					
-					} : {
+				
+								} : {
 						date : dataService.sqlDateFormate()
 					}, 
 					
@@ -147,16 +209,20 @@ define(['app'], function (app) {
 						console.log(table, input);
 						$rootScope.postData(table, input,function(response){
 							if(response.status == "success"){
-								$scope.getData(false, $scope.currentPage, 'staff','staff');
+								$scope.getData(false, $scope.currentPage, 'staff','staff',$Scope.staffParams);
 							}
 						})
 					},
 					updateData : function(table, input, id){
 						$rootScope.updateData(table, input, id, function(response){
 							if(response.status == "success"){
-								$scope.getData(false, $scope.currentPage, 'staff','staff');
+								$scope.getData(false, $scope.currentPage, 'staff','staff',$Scope.staffParams);
 							}
 						})
+					},
+					formPart : 'personalDetails',
+					showFormPart : function(formPart){
+						$scope.formPart = formPart;
 					}
 				};
 			
