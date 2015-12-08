31ed93a0083db27afe073f57a8c9301e520e4703
pragati wakale <pragati@wtouch.in>
2015-12-08 18:55:50 +0530

    flowers

--- website/templates/holidays_gifts_flowers/flowers/js/script.js
+++ website/templates/holidays_gifts_flowers/flowers/js/script.js
@@ -1,22 +1,7 @@
 'use strict';
 var hostUrl = '/website/templates/default/';
-jQuery(document).ready(function() {
-	jQuery(".subimgs").click(function(){
-		jQuery(".mainimgs").attr("src",(jQuery(this).attr("src")))
-	})
-});
 
-/* $(document).ready(function(){
-		if($(window).width() <= "540"){
-		$(window).scroll(function(e){
-			var scrollTop = $(document).scrollTop();
-			if(scrollTop > 20){
-				$('.navbar-default').removeClass('navbar-static-top').addClass("navbar-fixed-top");
-			} else {
-				$('.navbar-default').removeClass("navbar-fixed-top").addClass('navbar-static-top');
-			}
-		});
-	}
+$(document).ready(function(){
 	var sliderMode, minSlides, maxSlides;
 	//$(window).resize(function(){
 		if($(window).width() <= "480"){
@@ -26,8 +11,8 @@ jQuery(document).ready(function() {
 			
 		}else{
 			sliderMode = "vertical";
-			minSlides = 3;
-			maxSlides = 3;
+			minSlides = 4;
+			maxSlides = 4;
 		}
 	//})
 	$('.bxslider').bxSlider({
@@ -40,58 +25,35 @@ jQuery(document).ready(function() {
 		autoDirection:'next',
 		moveSlides: 1,
 		pause:3000,
-		pager:true,
+		pager:false,
 		pagerType:'full',
 		autoControls: true, 
 		controls:true, 
 		autoHover:true,
 		speed:1000,
 	});
-	$('.bxslider1').bxSlider({
-		mode: sliderMode,
-		slideWidth: 680,
-		minSlides: minSlides,
-		maxSlides: maxSlides,
-		slideMargin: 15,
-		auto: true, 
-		autoDirection:'next',
-		moveSlides: 1,
-		pause:3000,
-		pager:false,
-		pagerType:'full',
-		autoControls: false, 
-		controls:false, 
-		autoHover:true,
-		speed:1000,
-	});
-	$('.carousslider').bxSlider({ 
-		mode:'fade',
-		minSlides:1,
-		maxSlides: 1,
-		auto: true, 
-		autoDirection:'next',
-		pause:2500,
-		pager:false,
-		pagerType:'full',
-		autoControls: false, 
-		controls:false, 
-		autoHover:true,
-		speed:1000,
-	});
-}); */
-$(document).ready(function(){
 	jQuery(".subimgs").click(function(){
 		jQuery(".mainimgs").attr("src",(jQuery(this).attr("src")))
 	})
 	 $(window).scroll(function(e){
 		var scrollTop = $(document).scrollTop();
-		if(scrollTop > 35){
+		if(scrollTop > 0){
 			console.log(scrollTop);
 			$('nav').removeClass('navbar-static-top').addClass("navbar-fixed-top");
 		} else {
 			$('nav').removeClass("navbar-fixed-top").addClass('navbar-static-top')
 		}
 	}); 
+	$(window).scroll(function(e){
+		var scrollTop = $(document).scrollTop();
+		if(scrollTop > 800){
+			console.log(scrollTop);
+			$('featured_gallery').addClass("anim1");
+		} else {
+			$('featured_gallery').removeClass("anim1")
+		}
+	}); 
+	
  	// This is for Cookie
 	function getCookie(cname) {
 		var name = cname + "=";
@@ -104,13 +66,13 @@ $(document).ready(function(){
 		return false;
 	}
 	var cookieExpiry = new Date();
-	cookieExpiry.setMinutes(cookieExpiry.getSeconds() + 10);
+	cookieExpiry.setMinutes(cookieExpiry.getMinutes() + 30);
 	var cookieTime = cookieExpiry.toUTCString();
 	if(getCookie('anim') == false){
 		document.cookie="anim=true; expires=" + cookieTime;
 	}else{
-		$(".anim2, .anim4,.anim.js").hide();
-		$(".anim1").removeClass("anim1,anim.js");
+		$(".anim2, .anim4").hide();
+		$(".anim1").removeClass("anim1");
 		console.log(getCookie('anim'));
 	} 
 });
@@ -141,45 +103,28 @@ app.controller('enquiryController', function($scope,$http, $location) {
 		method: "GET",
 		params: params
 	}).then(function (results) {
+		console.log(results);
 		if(results.data.status == 'success'){
 			$scope.propertyConfig = results.data.data.config_data;
 		}else{
 			$scope.propertyConfig = results.data.data;
 		}
 	});
-	
-	// Don't change following code
-	/* start email code */
 	$scope.mailSent = false;
+
 	$scope.enquiry = {
+		message : {
+			property_for :0
+		},
+		subject : 'Website Enquiry',
 		date : year + "-" + month + "-" + date + " " + hour + ":" + min + ":"+sec
 	};
 	$scope.postData = function(enquiry){
 		$scope.loading = true;
-		if(enquiry.message.property_link){
-			enquiry.message.property_link = "<a href=\""+ location.origin +"/properties/"+ enquiry.message.property_link.params.property_title + "/" + enquiry.message.property_link.params.property_id + "\">"+enquiry.message.property_link.params.property_title+"</a>";
-		}
-		$http.post("/server-api/index.php/post/enquiry", $scope.enquiry).success(function(response) {
-			$scope.loading = false;
-			if(response.status == "success"){
-				$scope.mailSent = true;
-			}else{
-				$scope.mailSent = false;
-				$scope.errorMessage = response.message;
-				console.log(response);
-			}
+		console.log(enquiry);
+		 $http.post("/server-api/index.php/post/enquiry", $scope.enquiry).success(function(response) {
+			 $scope.loading = false;
+			$scope.mailSent = true;
 		});
 	};
-	/* End email code */
 });	
-
-	app.controller('aboutController', function($scope,$http, $location) {
-		var s = $location.path();
-		$scope.url = s.substr(1);
-		$scope.makeActive = function(url){
-			$scope.id = url;
-		}
-		$scope.makeActive($scope.url);
-		
-	});
-	 
