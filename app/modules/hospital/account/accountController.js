10f1648d22d8de15e19045a0662aab584352af1a
amar <amark@wtouch.in>
2015-12-19 18:15:52 +0530

    accountController
    staffpayment.html

--- app/modules/hospital/account/accountController.js
+++ app/modules/hospital/account/accountController.js
@@ -120,6 +120,7 @@ define(['app'], function (app) {
 					date : dataService.sqlDateFormate(),
 					user_id : $rootScope.userDetails.id,
 					date : dataService.sqlDateFormate(),
+					user_id : $rootScope.userDetails.id,
 					account_date: dataService.sqlDateFormate(),
 					date : dataService.sqlDateFormate(false,"datetime"),
 					modified_date : dataService.sqlDateFormate(false,"datetime"),
--- app/modules/hospital/general/ward/wardController.js
+++ app/modules/hospital/general/ward/wardController.js
@@ -65,6 +65,7 @@ define(['app'], function (app) {
 					} , 
 					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/hospital/general/ward/addward.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="Edit ward Information"> <span class="glyphicon glyphicon-pencil"></span></a>'
 					+ '<a type="button" tooltip="Delete ward" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'ward\', \'status\',row.entity.status, row.entity.id, grid.appScope.callbackColChange)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'
+					+ '<a ng-click="grid.appScope.openModal(\'modules/hospital/general/ward/wardview.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="view  ward" > <span class="glyphicon glyphicon glyphicon-eye-open"></span></a>'
 					
 				}
 			]
