123e3a049c8945d0d707b05a0ea6e3973a65c0b2
pallavi <pallavi@wtouch.in>
2015-12-23 04:21:10 -0800

    timetable changes

--- app/modules/campus/stock/stockController.js
+++ app/modules/campus/stock/stockController.js
@@ -1,34 +1,26 @@
 'use strict';
 
 define(['app'], function (app) {
-    var injectParams = ['$scope','$location','$rootScope','$injector','modalService','$routeParams' ,'$notification', 'dataService','uiGridConstants'];
+    var injectParams = ['$scope','$rootScope','$injector','modalService','$routeParams' ,'$notification', 'dataService', 'uiGridConstants'];
     
-    // This is controller for this view
-	var stockController= function ($scope,$location,$rootScope,$injector,modalService, $routeParams,$notification,dataService,uiGridConstants) {
-		
-		//global scope objects
-		$scope.type = "year";
+   var stockController = function ($scope,$rootScope,$injector,modalService, $routeParams,$notification,dataService, uiGridConstants) {
+		$rootScope.metaTitle = "campus";
 		$scope.maxSize = 5;
 		$scope.totalRecords = "";
-		$scope.pageItems = 10;
-		$scope.numPages = "";	
+		$scope.alerts = [];
 		$scope.currentPage = 1;
+		$scope.pageItems = 10;
 		$scope.currentDate = dataService.sqlDateFormate(false, "yyyy-MM-dd HH:MM:SS");
 		$rootScope.serverApiV2 = true;
 		$rootScope.module = "campus";
 		
-		$scope.vendorParams = {
-			where : {
-				status : 1,
-				user_id : $rootScope.userDetails.id
-			},
-			cols : ["*"]
-		}
+		
 		
 		$rootScope.moduleMenus = [
 			{
 				name : "Add stock",
-				SubTitle :"Add stock",
+				path : "#/dashboard/campus/stock",
+			
 				events : {
 					click : function(){
 						return $scope.openModal("modules/campus/stock/addstock.html");
@@ -36,38 +28,193 @@ define(['app'], function (app) {
 				}
 			}
 		]
+		var rowtpl='<div ng-class="{ \'my-css-class\': grid.appScope.rowFormatter( row ),\'text-success\':(row.entity.payment_status==1),\'text-danger\':(row.entity.payment_status==0),\'text-warning\':(row.entity.payment_status==2)}">' +
+                 '  <div ng-if="row.entity.merge">{{row.entity.title}}</div>' +
+                 '  <div ng-if="!row.entity.merge" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name" class="ui-grid-cell" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }"  ui-grid-cell></div>' +
+                 '</div>';
+		
+		$scope.stockData = {
+			enableSorting: true,
+			enableFiltering: true,
+			rowTemplate:rowtpl,
+			columnDefs: [
+				{ name:'SrNo', width:50,
+					enableSorting: false, enableFiltering: false,
+					cellTemplate : "<div class=\'ui-grid-cell-contents ng-binding ng-scope\'>{{ (grid.appScope.pageItems * (grid.appScope.currentPage - 1)) + rowRenderIndex + 1}}</div>",
+					
+				},
+				
+				{ name:'stock_id', width:70,enableSorting: false, enableFiltering: true,
+					filterHeaderTemplate: '<input id="stock_id" class="form-control" ng-change="grid.appScope.filter(\'stock_id\', stock_id, \'stock_view\', \'stockData\',true,grid.appScope.stockParams)" ng-model="stock_id" placeholder="stock No">',
+				},
+				{
+					name:'name',width :110,enableSorting: false,enableFiltering: true,
+					filterHeaderTemplate: '<select id="name" class="form-control" ng-change="grid.appScope.filter(\'vendor_id\', vendor_id, \'stock_view\', \'stockData\',true, grid.appScope.stockParams)" ng-model="vendor_id" ng-options="item.id as item.name for item in grid.appScope.vendorList">' 
+							+'<option value="">Select Vendor</option>'
+						+'</select>',
+				}, 
+			
+				{
+					name:'manage',width:200,enableSorting: false,enableFiltering: true,
+					filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'stock\', \'stockData\',false,grid.appScope.stockParams)" ng-model="status">'
+							 +'<option value="" selected>Status</option>' 
+							+'<option value="0">Deleted</option>'
+							+'<option value="1">Active</option>	'
+						+'</select>',
+					
+					
+					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/campus/stock/addstock.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="Edit stock"> <span class="glyphicon glyphicon-pencil"></span></a>'
+					
+					+ '<a type="button" tooltip="Delete stock" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'stock\', \'status\',row.entity.status, row.entity.id, grid.appScope.callbackColChange)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'
+					+
+					'<a ng-click="grid.appScope.openViewreceipt(\'modules/campus/stock/viewstock.html\',row.entity)" class="btn btn-warning btn-sm" type="button" tooltip-animation="true" tooltip="view stock Information"> <span class="glyphicon glyphicon-eye-open"></span></a>'
+					
+				}
+			],
+			onRegisterApi: function( gridApi ) {
+			  $scope.gridApi = gridApi;
+				$scope.gridApi.core.on.filterChanged( $scope, function() {
+					
+				})
+			}
+		};
+			
+		$scope.callbackColChange = function(response){
+			console.log(response);
+			if(response.status == "success"){
+				$scope.getData(false, $scope.currentPage, "stock", "stockData", $scope.stockParams);
+			}
+		}
 		
 		$scope.openModal = function(url,data){
-			var modalDefault = {
-				templateUrl: url,	// apply template to modal
+				var modalDefault = {
+				templateUrl: url, // apply template to modal
 				size : 'lg'
-			};
-			var modalOptions ={
-				addstock:(data)?{
-					id:data.id,
-					name:data.name
-				}:{},
-				postData : function(table, input){
-						console.log(table, input);
-						$rootScope.postData(table, input,function(response){
-							if(response.status == "success"){
-								$scope.getData(false, $scope.currentPage, 'stock','stock',$Scope.stockParams);
-							}
-						})
-					},
-					formPart : 'stockDetails',
-					showFormPart : function(formPart, modalOptions){
-						console.log(formPart);
-						modalOptions.formPart = formPart;
-					},
-					getData : $scope.getData,
-					
-	 };
-	 modalService.showModal(modalDefault, modalOptions).then(function(){
+				};
+			var modalOptions = {
+				date : dataService.sqlDateFormate(),
+				addstock : (data) ? {
+					id : data.id,
+					stock_id :data.stock_id,
+					vendor_id : data.vendor_id,
+					user_id : data.user_id,
+					type : data.type,
+					remark : data.remark,
+					particular : data.particular,
+					modified_date : dataService.sqlDateFormate(false,"datetime")
+				} : {
+					date : dataService.sqlDateFormate(false,"datetime"),
+					modified_date : dataService.sqlDateFormate(false,"datetime"),
 				
-		})
-	}
-	}		
+					status : 1,
+					user_id : $rootScope.userDetails.id
+				},
+				
+		
+				postData : function(table, input){
+					$rootScope.postData(table, input,function(response){
+						if(response.status == "success"){
+							// For Insert each item from particulars into Stock Table
+							
+							$scope.stockData = {};
+							$scope.stockData.user_id = input.user_id;
+							$scope.stockData.vendor_id = input.vendor_id;
+							if(input.date) $scope.stockData.date = input.date;
+							$scope.stockData.stockdate = input.generated_date;
+							$scope.stockData.modified_date = input.modified_date;
+							$scope.stockData.type = input.type; 
+							angular.forEach(input.particular, function(value, key){
+								$scope.stockData.goods_name = value.particular_name; 
+								$scope.stockData.quantity =  "+" + value.quantity;
+								$scope.stockData.goods_type = value.goods_type; 
+								$scope.stockData.category = value.category;
+								$rootScope.postData("stock", angular.copy($scope.stockData),function(response){
+								});
+							})						
+						$scope.getData(false, $scope.currentPage, 'stock','stockData',$scope.stockParams);
+						}
+					})
+				},
+				updateData : function(table, input, id){
+					$rootScope.updateData(table, input, id, function(response){
+						if(response.status == "success"){
+							$scope.getData(false, $scope.currentPage, 'stock','stockData',$scope.stockParams);
+						}
+					})
+				},
+				
+				getData: $scope.getData,
+				//addToObject : $rootScope.addToObject,
+				addToObject : function(object,data,modalOptions){
+					$rootScope.addToObject(object,modalOptions[data]);
+					modalOptions[data] = {};
+				},
+				
+				removeObject : $rootScope.removeObject
+			};
+			modalService.showModal(modalDefault, modalOptions).then(function(){
+			})
+		}
+		$scope.stockParams = {
+			where : {
+				status : 1,
+				user_id : $rootScope.userDetails.id
+			},
+			
+		}	
+			
+		// For Get (Select Data from DB)
+		$scope.getData = function(single, page, table, subobj, params, modalOptions) {
+			$scope.params = (params) ? params : {
+				where : {
+					status : 1,
+					user_id : $rootScope.userDetails.id
+				},
+				 
+				cols : ["*"]
+			};
+			if(page){
+				angular.extend($scope.params, {
+					limit : {
+						page : page,
+						records : $scope.pageItems
+					}
+				})
+			}
+			dataService.get(single,table,$scope.params).then(function(response) {
+				if(response.status == 'success'){
+					if(modalOptions != undefined){
+						modalOptions[subobj] = angular.copy(response.data);
+						modalOptions.totalRecords = response.totalRecords;
+					}else{
+						($scope[subobj]) ? $scope[subobj].data = angular.copy(response.data) : $scope[subobj] = angular.copy(response.data) ;
+						$scope.totalRecords = response.totalRecords;
+					}
+				}else{
+					if(modalOptions != undefined){
+						modalOptions[subobj] = [];
+						modalOptions.totalRecords = 0;
+					}else{
+						($scope[subobj]) ? $scope[subobj].data = [] : $scope[subobj] = [] ;
+						$scope.totalRecords = 0;
+					}
+				}
+			});
+		}
+		$scope.filter = function(col, value, table, subobj, search, params){
+			value = (value) ? value : undefined;
+			if(!params) params = {};
+			$rootScope.filterData(col, value, search, function(response){
+				dataService.extendDeep($scope.params, params, response);
+				$scope.getData(false ,$scope.currentPage, table, subobj, $scope.params);
+			})
+		}
+		$scope.orderBy = function(col, value){
+			if(!$scope.params.orderBy) $scope.params.orderBy = {};
+			$scope.params.orderBy[col] = value;
+			$scope.getData(false,$scope.currentPage, 'stock', 'stockData', $scope.params);
+		}
+	};	
 	// Inject controller's dependencies
 	stockController.$inject = injectParams;
 	// Register/apply controller dynamically
--- app/modules/campus/timetable/timetableController.js
+++ app/modules/campus/timetable/timetableController.js
@@ -52,27 +52,42 @@ define(['app'], function (app) {
 					
 				},
 				{ 
-					name:'dept_name',enableSorting: false ,
-					filterHeaderTemplate: '<select id="dept_name" class="form-control" ng-change="grid.appScope.filter(\'dept_name\', dept_name, \'timetable_view\', \'timetableList\',false, grid.appScope.timetableParams)" ng-model="dept_name" ng-options="item.dept_name as item.dept_name for item in grid.appScope.departmentList">'
+					name:'dept_name',width:60,enableSorting: false ,
+					filterHeaderTemplate: '<select id="dept_name" class="form-control" ng-change="grid.appScope.filter(\'dept_id\', dept_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="dept_id" ng-options="item.id as item.dept_name for item in grid.appScope.departmentList">'
+					+'<option value="" selected>Dept Name</option>'
+						+'</select>',
 				},
 				{ 
-					name:'class_name',enableSorting: false ,
-					filterHeaderTemplate: '<select id="name" class="form-control" ng-change="grid.appScope.filter(\'class_id\', class_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="class_id" ng-options="item.id as item.class_name for item in grid.appScope.classList">' 
+					name:'class_name',width:60,enableSorting: false ,
+					filterHeaderTemplate: '<select id="class_name" class="form-control" ng-change="grid.appScope.filter(\'class_id\', class_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="class_id" ng-options="item.id as item.class_name for item in grid.appScope.classList">' 
+					+'<option value="" selected>class Name</option>'
+						+'</select>',
 				},	
 				{ 
-					name:'division_name',enableSorting: false ,
-					filterHeaderTemplate: '<select id="name" class="form-control" ng-change="grid.appScope.filter(\'div_id\', div_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="div_id" ng-options="item.id as item.class_name for item in grid.appScope.classList">' 
+					name:'division_name',width:50,enableSorting: false ,
+					filterHeaderTemplate: '<select id="division_name" class="form-control" ng-change="grid.appScope.filter(\'div_id\', div_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="div_id" ng-options="item.id as item.division_name for item in grid.appScope.divisionList">' 
+					+'<option value="" selected>div Name</option>'
+						+'</select>',
+						
 				},				
-				 { name:'classRoom',enableSorting: false ,
-				filterHeaderTemplate: '<select id="room_id" class="form-control" ng-change="grid.appScope.filter(\'room_id\', room_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="room_id" ng-options="item.id as item.room_no for item in grid.appScope.roomList">'
+				{ name:'room_no',width:50,enableSorting: false ,
+				filterHeaderTemplate: '<select id="room_no" class="form-control" ng-change="grid.appScope.filter(\'room_id\', room_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="room_id" ng-options="item.id as item.room_no for item in grid.appScope.roomList">'
 							+'<option value="" selected>Room No</option>'
 						+'</select>',
+					cellTemplate:'<span>{{row.entity.multipleentries[0].room_id}}</span>'
 				},
 				
-				{ name:'Staff_name',enableSorting: false ,
-				filterHeaderTemplate: '<select id="staff_id" class="form-control" ng-change="grid.appScope.filter(\'staff_id\', staff_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="staff_id" ng-options="item.id as item.staff_name for item in grid.appScope.staffList">'
+				{ name:'name',enableSorting: false ,
+				filterHeaderTemplate: '<select id="name" class="form-control" ng-change="grid.appScope.filter(\'name\', name, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="name" ng-options="item.id as item.name for item in grid.appScope.staffList">'
 							+'<option value="" selected>Staff Name</option>'
 						+'</select>',
+							cellTemplate:'<span>{{row.entity.multipleentries[0].name}}</span>'
+				},
+				{ name:'sub_name',enableSorting: false ,
+				filterHeaderTemplate: '<select id="sub_name" class="form-control" ng-change="grid.appScope.filter(\'sub_id\', sub_id, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="sub_name" ng-options="item.id as item.sub_name for item in grid.appScope.subjectList">'
+							+'<option value="" selected>subject Name</option>'
+						+'</select>',
+						cellTemplate:'<span>{{row.entity.multipleentries[0].sub_name}}</span>'
 				},
 				
 				{ name:'date',
@@ -80,7 +95,7 @@ define(['app'], function (app) {
 					filterHeaderTemplate: '<input id="date" class="form-control" ng-change="grid.appScope.filter(\'date\', date, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableList)" ng-model="date" placeholder="date">',
 				},
 				{
-					name:'day',
+					name:'day',width:50,
 					enableSorting: false,
 					filterHeaderTemplate: '<select id="day" class="form-control" ng-change="grid.appScope.filter(\'day\', day, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams);grid.appScope.timetableday = grid.appScope.campusConfig[day]" ng-model="day">'
 							+'<option value="" selected>day</option>'
@@ -91,25 +106,28 @@ define(['app'], function (app) {
 							+'<option value="Thursday">Thursday</option>'
 							+'<option value="Friday">Friday</option>'
 							+'<option value="Saturday">Saturday</option>'
-						+'</select>'
+						+'</select>',
+						cellTemplate:'<span>{{row.entity.multipleentries[0].day}}</span>'
 				},
-				{ name:'timefrom',
-					filterHeaderTemplate: '<input id="time" class="form-control" ng-change="grid.appScope.filter(\'time\', time, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableList)" ng-model="timefrom" placeholder="timefrom">',
-				},
-				{ name:'timeto',
+				{ name:'timefrom',width:60,
+					filterHeaderTemplate: '<input id="time_from" class="form-control" ng-model="timefrom" placeholder="timefrom">',
+					cellTemplate : '<span>{{row.entity.multipleentries[0].timefrom}}</span>'
+				}, 
+				 { name:'timeto',width:60,
 				
-					filterHeaderTemplate: '<select id="timeto" class="form-control" ng-change="grid.appScope.filter(\'timeto\', timeto, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableList)" ng-model="timeto" placeholder="timeto">',
-				},
-				,	 
-				{ name:'Manage', 
-					filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'timetable_view\', \'timetableList\',false, grid.appScope.timetableParams)" ng-model="status">'
+					filterHeaderTemplate: '<select id="timeto" class="form-control" ng-model="timeto">',
+					cellTemplate : '<span>{{row.entity.multipleentries[0].timeto}}</span>'
+				}, 
+					 
+				{ name:'Manage',
+					filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'timetable_view\', \'timetableList\',true, grid.appScope.timetableParams)" ng-model="status">'
 							 +'<option value="" selected>Status</option>' 
 							+'<option value="0">Deleted</option>'
 							+'<option value="1">Active</option>	'
 						+'</select>',
 				
 					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/campus/timetable/addtimetable.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="Edit timetable" > <span class="glyphicon glyphicon-pencil"></span></a>'
-					+ '<a type="button" tooltip="Delete record" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'timetable\', \'status\',row.entity.status, row.entity.id, grid.appScope.callbackColChange)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'+ '<a ng-click="grid.appScope.openModal(\'modules/campus/timetable/viewtimetable.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="view  vendor" > <span class="glyphicon glyphicon glyphicon-eye-open"></span></a>'
+					+ '<a type="button" tooltip="Delete record" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'timetable\', \'status\',row.entity.status, row.entity.id, grid.appScope.callbackColChange)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'+ '<a ng-click="grid.appScope.openModal(\'modules/campus/timetable/viewtimetable.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip="view  timetable" ><span class="glyphicon glyphicon glyphicon-eye-open"></span></a>'
 					
 				}
 			]
@@ -135,6 +153,7 @@ define(['app'], function (app) {
 					id : data.id,
 					user_id : data.user_id,
 					timetable_id :data.timetable_id,
+					multipleentries : data.multipleentries,
 					dept_id:data.dept_id,
 					class_id :data.class_id,
 					div_id:data.div_id,
@@ -151,12 +170,38 @@ define(['app'], function (app) {
 					modified_date : dataService.sqlDateFormate(false,"datetime"),
 					dept_id : 1,
 				},
-				postData : function(table, input){
-					$rootScope.postData(table, input,function(response){
-						if(response.status == "success"){
-							$scope.getData(false, $scope.currentPage, 'timetable_view','timetableList',$scope.timetableParams);
-						}
-					})
+			view : (data)?{
+				dept_name :  data.dept_name,
+				class_name: data.class_name,
+				division_name: data.division_name,
+				room_no: data.room_no,
+				name: data.name,
+				sub_name:data.sub_name,
+				
+			}:
+			{
+				
+			},
+			postData : function(table, input){
+				console.log(input);
+					$scope.timetableData = {};
+					$scope.timetableData.user_id =input.user_id;
+					$scope.timetableData.dept_id =input.dept_id;
+					$scope.timetableData.class_id =input.class_id;
+					$scope.timetableData.div_id =input.div_id;
+					angular.forEach(input.multipleentries, function(value, key){
+								$scope.timetableData.sub_id = value.sub_id;
+								$scope.timetableData.room_no = value.room_no;
+								$scope.timetableData.name = value.name;
+								$scope.timetableData.day = value.day;
+								$scope.timetableData.date = value.date;
+								$scope.timetableData.timefrom = value.timefrom;
+								$scope.timetableData.timeto = value.timeto;
+								$rootScope.postData("timetable", angular.copy($scope.timetableData),function(response){
+									$scope.getData(false, $scope.currentPage, 'timetable_view','timetableList',$scope.timetableParams); 
+								});
+							})
+						
 				},
 				updateData : function(table, input, id){
 					$rootScope.updateData(table, input, id, function(response){
@@ -164,7 +209,7 @@ define(['app'], function (app) {
 							$scope.getData(false, $scope.currentPage, 'timetable_view','timetableList',$scope.timetableParams);
 						}
 					})
-				},
+				}, 
 				getData : $scope.getData,
 				classParams : {
 					where : {
@@ -172,7 +217,13 @@ define(['app'], function (app) {
 					status:1,
 				},
 				cols : ["*"]
-				}
+				},
+				addToObject : function(object,data,modalOptions){
+					$rootScope.addToObject(object,modalOptions[data]);
+					modalOptions[data] = {};
+				},
+				
+				removeObject : $rootScope.removeObject
 			};
 			
 			modalService.showModal(modalDefault, modalOptions).then(function(){
@@ -180,7 +231,6 @@ define(['app'], function (app) {
 			})
 	}	
 	$scope.getData = function(single, page, table, subobj, params, modalOptions) {
-		console.log(params);
 			$scope.params = (params) ? params : {
 				where : {
 					user_id : $rootScope.userDetails.id,
