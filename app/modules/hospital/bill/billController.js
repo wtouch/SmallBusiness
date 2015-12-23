290ef9f361560187d9b3244c0d051b712d405ec7
manisha karande <manisha@wtouch.in>
2015-12-23 16:16:17 +0530

    view bill

--- app/modules/hospital/bill/billController.js
+++ app/modules/hospital/bill/billController.js
@@ -21,127 +21,66 @@ define(['app'], function (app) {
 			popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" /><link rel="stylesheet" type="text/css" href="css/style.css" /></head><body onload="window.print()">' + printContents + '</html>');
 			popupWin.document.close();
 		}
-		$scope.billData = {
-			enableSorting: true,
-			enableFiltering: true,
-			rowTemplate:rowtpl,
-			columnDefs: [
-				{ name:'SrNo', width:50,
-					cellTemplate : "<span>{{ (grid.appScope.pageItems * (grid.appScope.currentPage - 1)) + rowRenderIndex + 1}}</span>",enableSorting: false, enableFiltering: false,
-					
-				},
-				
-				{ name:'bill_id', width:70,enableSorting: false, enableFiltering: true,
-					filterHeaderTemplate: '<input id="bill_id" class="form-control" ng-change="grid.appScope.filter(\'bill_id\', bill_id, \'bill\', \'billData\',true,grid.appScope.billParams)" ng-model="bill_id" placeholder="Bill No">',
-				},
-				
-			
-				
-				
-				
-				
-				{
-					name:'manage',width:200,enableSorting: false,enableFiltering: true,
-					filterHeaderTemplate: '<select id="status" class="form-control" ng-change="grid.appScope.filter(\'status\', status, \'bill\', \'billData\',false,grid.appScope.billParams)" ng-model="status">'
-							 +'<option value="" selected>Status</option>' 
-							+'<option value="0">Deleted</option>'
-							+'<option value="1">Active</option>	'
-						+'</select>',
-					
-					
-					cellTemplate : '<a ng-click="grid.appScope.openModal(\'modules/hospital/bill/generate_bill.html\',row.entity)" class="btn btn-primary btn-sm" type="button" tooltip-animation="true" tooltip=" Generate bill Information"> <span class="glyphicon glyphicon-pencil"></span></a>'
-					
-					+ '<a type="button" tooltip="Delete stock" ng-class="(row.entity.status==1) ? \'btn btn-success btn-sm\' : \'btn btn-danger btn-sm\'" ng-model="row.entity.status" ng-change="grid.appScope.changeCol(\'bill\', \'status\',row.entity.status, row.entity.id, grid.appScope.callbackColChange)" btn-checkbox="" btn-checkbox-true="\'1\'" btn-checkbox-false="\'0\'" class="ng-pristine ng-valid active btn btn-success btn-sm"><span class="glyphicon glyphicon-remove"></span></a>'
-			'
-					
+		$rootScope.moduleMenus = [
+			{
+				name : "Generate Bill",
+				path : "#/dashboard/hospital/bill",
+				events : {
+					click : function(){
+						return $scope.openModal("modules/hospital/bill/generate_bill.html");
+					}
 				}
-			],
-			onRegisterApi: function( gridApi ) {
-			  $scope.gridApi = gridApi;
-				$scope.gridApi.core.on.filterChanged( $scope, function() {
-					
-				})
 			}
-		};
-		
-		$scope.callbackColChange = function(response){
-			console.log(response);
-			if(response.status == "success"){
-				$scope.getData(false, $scope.currentPage, "bill", "billData", $scope.billParams);
-			}
-		}
-		
+		]
 		
 		$scope.openModal = function(url,data){
-				var modalDefault = {
-				templateUrl: url, // apply template to modal
+			var modalDefault = {
+				templateUrl:url,	// apply template to modal
 				size : 'lg'
-				};
+			};
 			var modalOptions = {
-				date : dataService.sqlDateFormate(),
-				addBill : (data) ? {
+				date : $scope.currentDate,
+				//equipment_date:$scope.currentDate,
+				modified_date:$scope.currentDate,
+				date:$scope.currentDate,
+				addequipment : (data) ? {
 					id : data.id,
-					bill_id :data.bill_id,
-				
-					modified_date : dataService.sqlDateFormate(false,"datetime")
+					user_id : data.user_id,
+					user_id : $rootScope.userDetails.id,
+					modified_date:data.modified_date,
+					equipment_date : data.equipment_date,
+					price:data.price,
+					quantity:data.quantity,
+					equipment_name:data.equipment_name,
+					unit:data.unit,
+					equipment_charges:data.equipment_charges,
+					equipment_description:data.equipment_description
 				} : {
 					date : dataService.sqlDateFormate(false,"datetime"),
+					user_id : $rootScope.userDetails.id,
 					modified_date : dataService.sqlDateFormate(false,"datetime"),
-					due_date : $scope.setDate(dataService.sqlDateFormate(), 10, "date"),
-					status : 1,
-					user_id : $rootScope.userDetails.id
-				},
+					equipment_date: dataService.sqlDateFormate()
 				
-				getBalance : $scope.getBalance,
-				calcBalance : $scope.calcBalance,
-				receiptData : (data) ?{
-					name : data.name
-				}:{
-					
 				},
-				receiptParams : (data) ?{
-					where : {
-						reference_id : data.id,
-						type : "bill_payment",
-						user_id : $rootScope.userDetails.id,
-						status : 1
-					},
-					cols : ["*"]
-				}:{
-
-				},
-				
-				updateData : function(table, input, id){
-					$rootScope.updateData(table, input, id, function(response){
+				postData : function(table, input){
+					$rootScope.postData(table, input,function(response){
 						if(response.status == "success"){
-							$scope.getData(false, $scope.currentPage, 'bill','billData',$scope.billParams);
+							$scope.getData(false, $scope.currentPage, 'equipment','equipmentList');
 						}
 					})
 				},
-				setDate : function(date, days){
-					var dueDate = $scope.setDate(date, days, "date");
-					modalOptions.addBill.due_date = dueDate;
+				updateData : function(table, input, id){
+					$rootScope.updateData(table, input, id, function(response){
+						if(response.status == "success"){
+							$scope.getData(false, $scope.currentPage, 'equipment','equipmentList');
+						}
+					})
 				},
-				sqlDateFormat : function(date, object){
-					var sqlDate = dataService.sqlDateFormate(date,"date");
-					object = sqlDate;
-				},
-				getData: $scope.getData,
-				//addToObject : $rootScope.addToObject,
-				addToObject : function(object,data,modalOptions){
-					$rootScope.addToObject(object,modalOptions[data]);
-					modalOptions[data] = {};
-				},
-				
-				removeObject : $rootScope.removeObject
+				getData : $scope.getData
 			};
-			modalService.showModal(modalDefault, modalOptions).then(function(){
+			modalService.showModal(modalDefault, modalOptions).then(function(){	
 			})
 		}
-		
-		
-		
-		
 		$scope.billParams = {
 			where : {
 				status : 1,
